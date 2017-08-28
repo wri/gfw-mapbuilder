4201c142ee2faad31ae3bc8d087058afce44184f
Patrick Moran <pmoran@blueraster.com>
2017-08-28 12:09:01 -0400

    add commas to numbers in analysis badges

--- src/js/components/AnalysisPanel/LossGainBadge.js
+++ src/js/components/AnalysisPanel/LossGainBadge.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import text from 'js/languages';
 import { analysisConfig } from 'js/config';
+import utils from 'utils/AppUtils';
 import analysisKeys from 'constants/AnalysisConstants';
 
 const LossGainBadge = (props, context) => {
@@ -25,13 +26,13 @@ const LossGainBadge = (props, context) => {
         <div className='results__loss-badge'>
           <div className='results__loss-gain--label'>{text[language].ANALYSIS_TOTAL_LOSS_LABEL}</div>
           <div className='results__loss-gain--range'>{labelArray[lossFromSelectIndex]} &ndash; {labelArray[lossToSelectIndex]}</div>
-          <span className='results__loss--count'>{lossTotal}</span>
+          <span className='results__loss--count'>{utils.formatNumber(lossTotal)}</span>
           <span className='results__loss--unit'>Ha</span>
         </div>
         <div className='results__gain-badge'>
           <div className='results__loss-gain--label'>{text[language].ANALYSIS_TOTAL_GAIN_LABEL}</div>
           <div className='results__loss-gain--range'>{text[language].ANALYSIS_TOTAL_GAIN_RANGE}</div>
-          <span className='results__gain--count'>{gainTotal}</span>
+          <span className='results__gain--count'>{utils.formatNumber(gainTotal)}</span>
           <span className='results__gain--unit'>Ha</span>
         </div>
       </div>
