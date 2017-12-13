6f5dbf07352510a1b440eb4aca016d603e9f53d1
Lucas Cotner <lc07@uw.edu>
2017-12-13 12:24:53 -0500

    Indigenous Lands Report via dropdown

---
+++ src/js/components/AnalysisPanel/RedirectChart.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+export default class RedirectChart extends Component {
+
+  componentDidMount() {
+    const openWindow = window.open('/country-profiles/');
+    console.log('openWindow', openWindow);
+
+    if (!openWindow || typeof openWindow === 'undefined') {
+      alert('Turn off your pop-up blocker!');
+    }
+  }
+
+  render() {
+    return (
+      <div className='analysis__carbon-container flex'>
+        <div className='chart-error'>View results in new tab</div>
+      </div>
+    );
+  }
+}
