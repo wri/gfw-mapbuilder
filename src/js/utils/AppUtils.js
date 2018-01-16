7f59748fe5e78c0c164188402cf0e5d76219eede
Patrick Moran <pmoran@blueraster.com>
2018-01-16 12:00:26 -0500

    update AppUtils.js

--- src/js/utils/AppUtils.js
+++ src/js/utils/AppUtils.js
@@ -269,7 +269,7 @@ const utils = {
 
   formatNumber: (number) => {
 
-    const numberString = number.toString();
+    const numberString = Math.round(number).toString();
 
     if (number >= 1000) {
       const stringArray = [];
