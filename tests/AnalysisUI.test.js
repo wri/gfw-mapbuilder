e220342c3354aba60af76f01fb3f8f05d7644e86
Lucas Cotner <lc07@uw.edu>
2018-02-14 17:33:51 -0500

    Passing in the proper props for analysis and component to get our analysisUI test working

--- tests/AnalysisUI.test.js
+++ tests/AnalysisUI.test.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import { shallow } from 'enzyme';
 import AnalysisTypeSelect from '../src/js/components/AnalysisPanel/AnalysisTypeSelect';
 import AnalysisMultiDatePicker from '../src/js/components/AnalysisPanel/AnalysisFormElements/AnalysisMultiDatePicker';
+
+
+const context = {
+  language: 'en',
+  settings: {}
+};
 
 describe('<AnalysisTypeSelect />', () => {
   it('should render <AnalysisMultiDatePicker /> when multi is true', () => {
     const testAnalysisModules = [
       {
+        analysisId: 'GLAD_ALERTS',
+        label: {
+          en: 'GLAD Alerts'
+        },
         uiParams: [
           {
             inputType: 'datepicker',
@@ -14,7 +25,8 @@ describe('<AnalysisTypeSelect />', () => {
         ],
       },
     ];
-    const wrapper = shallow(<AnalysisTypeSelect analysisItems={testAnalysisModules} />);
+    const wrapper = shallow(<AnalysisTypeSelect activeAnalysisType='default' analysisItems={testAnalysisModules} />, { context });
+    wrapper.setContext(context);
     expect(wrapper.find(AnalysisMultiDatePicker));
   });
 });
