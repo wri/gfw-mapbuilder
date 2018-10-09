b4730c170af1923d922553eaa4c15a9e5aaca00d
Lucas Cotner <lc07@uw.edu>
2018-10-09 11:37:29 -0400

    Updated e2e url

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -10,7 +10,7 @@ describe('My first integration test', function () {
 describe('An actual test on our app', function () {
   it('Ensures our external constructor has the correct properties', function () {
 
-    cy.visit('http://localhost:8000/libBuild/external.html');
+    cy.visit('https://beta.blueraster.io/mapbuilder/eth-report-persists/external.html');
 
     cy.window().then((win) => {
       // win is the remote window
@@ -38,7 +38,7 @@ describe('An actual test on our app', function () {
 
   it('Tests our library constructor parameters', function () {
 
-    cy.visit('http://localhost:8000/libBuild/external.html');
+    cy.visit('https://beta.blueraster.io/mapbuilder/eth-report-persists/external.html');
 
     cy.window().then((win) => { // win is the remote window
 
@@ -119,6 +119,90 @@ describe('An actual test on our app', function () {
             expect(layer).to.have.property('label');
             expect(layer.label).toHaveProperty(config.language);
           });
+        });
+      }
+
+      if (config.analysisModules) {
+        config.analysisModules.forEach((module) => {
+          expect(module.label).toHaveProperty(config.language);
+          expect(module).toHaveProperty('analysisId');
+
+          if (module.useGfwWidget) {
+            expect(module).toHaveProperty('widgetId');
+            expect(module).not.toHaveProperty('chartType');
+          } else {
+            expect(module).toHaveProperty('chartType');
+          }
+
+          if (module.params) {
+            expect(module.params).toBeInstanceOf(Array);
+
+            module.params.forEach((param) => {
+              expect(param).toHaveProperty('name');
+              expect(param).toHaveProperty('value');
+            });
+          }
+
+          if (module.analysisId !== 'LCC') {
+            expect(module.chartType).not.toEqual('lccPie');
+          }
+
+          if (module.analysisId !== 'BIO_LOSS') {
+            expect(module.chartType).not.toEqual('biomassLoss');
+          }
+
+          if (module.analysisId !== 'TC_LOSS_GAIN' && module.analysisId !== 'VIIRS_FIRES' && module.chartType === 'badge') {
+            expect(module).toHaveProperty('badgeLabel');
+            expect(module.badgeLabel).toHaveProperty(config.language);
+          }
+
+          if (module.chartType === 'bar') {
+            expect(module).toHaveProperty('chartBounds');
+            expect(module.chartBounds).toBeInstanceOf(Array);
+            expect(module.chartBounds).toHaveLength(2);
+          }
+
+          if (typeof module.uiParams === 'string') {
+            expect(module.uiParams).toEqual('none');
+          } else {
+            expect(module.uiParams).toBeInstanceOf(Array);
+
+            module.uiParams.forEach((uiParam) => {
+              expect(uiParam.label).toHaveProperty(config.language);
+              expect(uiParam).toHaveProperty('inputType');
+
+              if (uiParam.inputType === 'tcd') {
+                expect(uiParam).toHaveProperty('name');
+              } else if (uiParam.inputType === 'rangeSlider') {
+                expect(uiParam).toHaveProperty('startParamName');
+                expect(uiParam).toHaveProperty('bounds');
+                expect(uiParam.bounds).toBeInstanceOf(Array);
+                expect(uiParam.bounds).toHaveLength(2);
+                expect(uiParam).toHaveProperty('combineParams');
+
+                if (uiParam.combineParams) {
+                  expect(uiParam).toHaveProperty('valueSeparator');
+                } else {
+                  expect(uiParam).toHaveProperty('endParamName');
+                }
+              } else if (uiParam.inputType === 'datepicker') {
+                expect(uiParam).toHaveProperty('startParamName');
+                expect(uiParam).toHaveProperty('multi');
+
+                if (uiParam.multi) {
+                  expect(uiParam).toHaveProperty('combineParams');
+
+                  if (uiParam.combineParams) {
+                    expect(uiParam).toHaveProperty('valueSeparator');
+                  } else {
+                    expect(uiParam).toHaveProperty('endParamName');
+                  }
+                }
+
+                if (uiParam.minDate) { expect(uiParam.minDate).toMatch(/\d{4}-\d{2}-\d{2}/); }
+              }
+            });
+          }
         });
       }
 
