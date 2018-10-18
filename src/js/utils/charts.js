4b780fbe9e909d840cd3771d318b9f6553daa6ae
Lucas Cotner <lc07@uw.edu>
2018-10-18 13:02:42 -0400

    Updated wcs call and errback to fake the call

--- src/js/utils/charts.js
+++ src/js/utils/charts.js
@@ -398,7 +398,8 @@ export default {
         render(JSON.parse(config));
       }).catch(err => {
         console.log('err', err);
-        debugger
+        // debugger
+        render(JSON.parse('{"$schema":"https://vega.github.io/schema/vega/v4.json","width":200,"height":100,"padding":5,"title":"Population Trend","data":[{"name":"table","values":[{"surveyYear":2002,"siteAbundance":2731},{"surveyYear":2004,"siteAbundance":2652},{"surveyYear":2006,"siteAbundance":2175},{"surveyYear":2010,"siteAbundance":2324}]}],"signals":[{"name":"maxbins","value":10,"bind":{"input":"select","options":[5,10,20]}},{"name":"tooltip","value":{},"on":[{"events":"rect:mouseover","update":"datum"},{"events":"rect:mouseout","update":"{}"}]}],"scales":[{"name":"xscale","type":"band","domain":{"data":"table","field":"surveyYear"},"range":"width","padding":0.05,"round":true},{"name":"yscale","domain":{"data":"table","field":"siteAbundance"},"nice":true,"range":"height"}],"axes":[{"orient":"bottom","scale":"xscale","title":"Year"},{"orient":"left","scale":"yscale","title":"Number"}],"marks":[{"type":"rect","from":{"data":"table"},"encode":{"enter":{"x":{"scale":"xscale","field":"surveyYear"},"width":{"scale":"xscale","band":1},"y":{"scale":"yscale","field":"siteAbundance"},"y2":{"scale":"yscale","value":0}},"update":{"fill":{"value":"steelblue"}},"hover":{"fill":{"value":"red"}}}},{"type":"text","encode":{"enter":{"align":{"value":"center"},"baseline":{"value":"bottom"},"fill":{"value":"#333"}},"update":{"x":{"scale":"xscale","signal":"tooltip.surveyYear","band":0.5},"y":{"scale":"yscale","signal":"tooltip.siteAbundance","offset":-2},"text":{"signal":"tooltip.siteAbundance"},"fillOpacity":[{"test":"datum === tooltip","value":0},{"value":1}]}}}]}'));
       });
 
     // esriRequest({
@@ -413,16 +414,16 @@ export default {
     //   console.log('err', err);
     // });
 
-    // function render(spec) {
-    //   console.log('spec', spec);
-    //   console.log('config', config);
-    //   debugger
-    //   view = new vega.View(vega.parse(spec))
-    //     .renderer('canvas')  // set renderer (canvas or svg)
-    //     .initialize(el) // initialize view within parent DOM container
-    //     .hover()             // enable hover encode set processing
-    //     .run();
-    // }
+    function render(spec) {
+      console.log('spec', spec);
+      console.log('config', config);
+      // debugger
+      new vega.View(vega.parse(spec))
+        .renderer('canvas')  // set renderer (canvas or svg)
+        .initialize(el) // initialize view within parent DOM container
+        .hover()             // enable hover encode set processing
+        .run();
+    }
   },
 
 	/**
