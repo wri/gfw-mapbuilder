775bc69a99fe68d18210fe264b18d5a791b80a3f
Jennifer Boyd <jboyd@blueraster.com>
2018-11-06 13:51:00 -0500

    Add draggable modal

---
+++ src/js/components/Modals/DraggableModalWrapper.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import SVGIcon from 'utils/svgIcon';
+
+/**
+* Should be wrapped in a component with relative or absolute position
+*/
+export default function DraggableModalWrapper (props) {
+  const contentClass = `modal-content custom-scroll ${props.theme ? props.theme : ''}`;
+  return (
+    <article id='draggableModal' className='modal draggable' draggable='true' onDragEnd={props.onDragEnd}>
+      <div title='close' className='close-icon pointer' onClick={props.onClose} >
+        <svg>
+          <SVGIcon id={'shape-close'} />
+        </svg>
+      </div>
+        <div className={contentClass}>
+          {props.children}
+        </div>
+    </article>
+  );
+}
+
+DraggableModalWrapper.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  theme: PropTypes.string
+};
--- src/js/components/Modals/ImageryModal.js
+++ src/js/components/Modals/ImageryModal.js
@@ -1,4 +1,4 @@
-import ControlledModalWrapper from 'components/Modals/ControlledModalWrapper';
+import DraggableModalWrapper from 'components/Modals/DraggableModalWrapper';
 import mapActions from 'actions/MapActions';
 import React, { Component } from 'react';
 
@@ -8,11 +8,25 @@ export default class ImageryModal extends Component {
     mapActions.toggleImageryVisible(false);
   };
 
+  onDragEnd = (event) => {
+    event.target.style.top = event.clientY;
+    event.target.style.left = event.clientX;
+  };
+
   render () {
     return (
-      <ControlledModalWrapper onClose={this.close}>
-      Imagery Modal
-      </ControlledModalWrapper>
+      <DraggableModalWrapper onClose={this.close} onDragEnd={this.onDragEnd}>
+        <div className='imagery-modal__wrapper'>
+          <div className='imagery-modal__title'>Recent Hi-Res Satellite Imagery</div>
+
+          <div className='imagery-modal__section filters'></div>
+
+          <div className='imagery-modal__section'></div>
+
+        </div>
+
+
+      </DraggableModalWrapper>
     );
   }
 
