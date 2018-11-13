8992227ac4a0dc32f0a5d96c5fbdad77cf0046f7
Nick Bumbarger <nbumbarger@gmail.com>
2018-11-13 13:45:42 -0500

    Add choropleth legend type

--- src/js/components/LegendPanel/ApiLegend.js
+++ src/js/components/LegendPanel/ApiLegend.js
@@ -8,6 +8,59 @@ export default class ApiLegend extends Component {
       visible: props.visibility,
       opacity: props.defaultOpacity
     };
+
+    this.generateLegends = this.generateLegends.bind(this);
+  }
+
+
+  generateLegends(items, type, language) {
+    switch(type) {
+      case 'choropleth':
+        return items.map((item, i) => {
+          const { name, color, outlineColor } = item;
+          console.log(outlineColor);
+            return (
+              <div className='legend-row' key={`webmap-legend-row-${name[language]}-${i}`}>
+                <div style={{
+                  backgroundColor: color,
+                  border: `1px solid ${outlineColor}`,
+                  opacity: this.state.opacity
+                }} className='legend-icon'></div>
+                <div className='legend-label'>{name[language]}</div>
+              </div>
+            );
+          }
+        );
+
+    }
+  }
+
+  render () {
+    const visible = this.state.visible;
+    const { label, metadata, language } = this.props;
+    const { name, type, items } = metadata.legendConfig;
+
+    return (
+      <div className={`parent-legend-container ${visible && 'hidden'}`} ref='myRef' key={`webmap-legend-${name[language]}`}>
+        <div className='label-container'>
+          <strong>{name[language]}</strong>
+        </div>
+        <div className='legend-container'>
+          {items.length &&
+            <div className='crowdsource-legend'>
+              {this.apiItemMapper(items, type, language)}
+            </div>}
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.props.initialLayerOpacities.forEach(opacity => {
+      if (opacity.layerId === this.props.layerId) {
+        this.setState({ opacity: opacity.value });
+      }
+    });
   }
 
   componentDidUpdate(prevProps) {
@@ -22,18 +75,5 @@ export default class ApiLegend extends Component {
     if (this.props.legendOpacity.layerId === this.props.layerId && this.props.legendOpacity.value !== prevProps.legendOpacity.value) {
       this.setState({ opacity: this.props.legendOpacity.value });
     }
-  }
-
-  componentDidMount() {
-    this.props.initialLayerOpacities.forEach(opacity => {
-      if (opacity.layerId === this.props.layerId) {
-        this.setState({ opacity: opacity.value });
-      }
-    });
-  }
-
-  render () {
-    const { label, metadata, language } = this.props;
-
   }
 }
