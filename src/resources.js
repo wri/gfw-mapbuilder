9137f02d7edecec1b22b6840a32b93964dd8bb12
Lucas Cotner <lc07@uw.edu>
2019-03-21 12:46:57 -0400

    Example property overrides for layers registered w/ GFW API

--- src/resources.js
+++ src/resources.js
@@ -618,7 +618,15 @@ export default {
       layers: [{
         order: 1,
         type: 'remoteDataLayer',
-        uuid: '0721f089-b887-4d49-bad3-4b19261de208'
+        uuid: '0721f089-b887-4d49-bad3-4b19261de208',
+        lucas: 'cotnaaa',
+        visible: true,
+        sublabel: {
+          en: 'new sublabellll'
+        },
+        label: {
+          en: 'Tree Cover Lost'
+        }
       }, {
         order: 2,
         type: 'remoteDataLayer',
