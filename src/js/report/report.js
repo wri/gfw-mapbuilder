7c31a8dde1ba5f24b4f9ca8297f3c53ff3c74009
Kayla Kremer <kremer.kayla@gmail.com>
2019-06-24 15:13:14 -0400

    Add chart descriptions to report

--- src/js/report/report.js
+++ src/js/report/report.js
@@ -989,11 +989,15 @@ const runAnalysis = function runAnalysis (params, feature) {
       //label.innerHTML = module.label.en;
       //resultsContainer.appendChild(label);
       resultsContainer.appendChild(div);
+      const infoDiv = document.createElement('div');
+      infoDiv.classList.add('vega-chart-info');
+      infoDiv.innerHTML = module.description[language];
       analysisUtils.getCustomAnalysis(module, uiParamsToAppend).then(results => {
-
+        
         const chartComponent = renderResults(results, language, module, params);
         const moduleDiv = document.getElementById(module.analysisId + '_div');
         ReactDOM.render(chartComponent, moduleDiv);
+        moduleDiv.appendChild(infoDiv);
       });
       return;
     }
