c4169ee18d9821ccac067774a307a6fb7fb5ed0c
Lucas Cotner <lc07@uw.edu>
2019-07-30 12:38:47 -0400

    e2e tests for AGOL appid overrides

---
+++ cypress/integration/agol_spec.js
@@ -0,0 +1,62 @@
+
+describe('An actual test on our app', function () {
+
+  it("Tests our AGOL app's overwrite of our constructor parameters", function () {
+
+    cy.visit('https://my.gfw-mapbuilder.org/v1.4.0/?appid=277b92455c6c49059a95c9129be7baf9&l=en');
+
+    cy.window().then(windowObject => {
+      const app = windowObject.customApp;
+      expect(app).to.not.be.an('undefined');
+
+      const constructorParams = app.constructorArgs;
+      const config = constructorParams.config;
+      cy.wait(2000);
+
+      cy.get('.app-header__title').should(($div) => {
+        const titleText = $div.get(0).innerText;
+        expect(titleText.toLowerCase()).to.not.eq(config.title.toLowerCase());
+      });
+      cy.get('.app-header__subtitle').should(($div) => {
+        const subtitleText = $div.get(0).innerText;
+        expect(subtitleText.toLowerCase()).to.not.eq(config.subtitle.toLowerCase());
+      });
+
+      cy.get('.app-header__nav-list').children().should('have.length', 5);
+
+      cy.get('.app-header__logo-container').find('img').should('have.attr', 'src').should('not.include', config.logoUrl);
+
+    });
+
+  });
+
+  it("Tests our site's html for our constructor parameters", function () {
+
+    cy.visit('https://my.gfw-mapbuilder.org/v1.4.0/');
+
+    cy.window().then(windowObject => {
+      const app = windowObject.customApp;
+      expect(app).to.not.be.an('undefined');
+
+      const constructorParams = app.constructorArgs;
+      const config = constructorParams.config;
+      cy.wait(2000);
+
+      cy.get('.app-header__title').should(($div) => {
+        const titleText = $div.get(0).innerText;
+        expect(titleText.toLowerCase()).to.eq(config.title.toLowerCase());
+      });
+      cy.get('.app-header__subtitle').should(($div) => {
+        const subtitleText = $div.get(0).innerText;
+        expect(subtitleText.toLowerCase()).to.eq(config.subtitle.toLowerCase());
+      });
+
+      cy.get('.app-header__nav-list').children().should('have.length', 1);
+
+      cy.get('.app-header__logo-container').find('img').should('have.attr', 'src').should('include', config.logoUrl);
+
+    });
+
+  });
+
+});
