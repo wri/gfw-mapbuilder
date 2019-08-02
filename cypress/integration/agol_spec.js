53d17ecdc7ca7a5d7f85c0addbbbcea146bb98eb
Lucas Cotner <lc07@uw.edu>
2019-08-02 14:53:54 -0400

    Mid-test stashin!

--- cypress/integration/agol_spec.js
+++ cypress/integration/agol_spec.js
@@ -11,7 +11,7 @@ describe('An actual test on our app', function () {
 
       const constructorParams = app.constructorArgs;
       const config = constructorParams.config;
-      cy.wait(3000);
+      cy.wait(11000);
 
       cy.get('.app-header__title').should(($div) => {
         const titleText = $div.get(0).innerText;
@@ -39,73 +39,79 @@ describe('An actual test on our app', function () {
       cy.get('[data-value="NARRATIVE"]').should('have.length', 1);
       cy.get('[data-value="NARRATIVE"]').should('have.class', 'active');
       cy.get('.tab-view__content').first().should('have.class', 'selected');
-      cy.get('.tab-view__narrative').should(($div) => {
-        const divText = $div.get(0).innerText;
-        expect(divText.toLowerCase()).to.eq('This is a test mapbuilder');
-      });
-      cy.get('.tab-view__narrative').should(($div) => {
-        const divText = $div.get(1).innerText;
-        expect(divText.toLowerCase()).to.eq('This text is bold');
-      });
+      // cy.get('.tab-view__narrative').should(($div) => {
+      //   const divText = $div.get(0).innerText;
+      //   expect(divText.toLowerCase()).to.eq('This is a test mapbuilder');
+      // });
+      // cy.get('.tab-view__narrative').should(($div) => {
+      //   const divText = $div.get(1).innerText;
+      //   expect(divText.toLowerCase()).to.eq('This text is bold');
+      // });
+      // const ll = cy.get('tab-view__narrative div').first();
+      // console.log(ll);
 
       cy.get('.control-panel__share-map').should('have.length', 1);
 
+      cy.get('[data-value="LAYERS"]').click();
+      cy.get('[data-value="LAYERS"]').should('have.class', 'active');
 
-
-    });
-
-  });
-
-  it("Tests our site's html for our constructor parameters", function () {
-
-    cy.visit('https://my.gfw-mapbuilder.org/v1.4.0/');
-
-    cy.window().then(windowObject => {
-      const app = windowObject.customApp;
-      expect(app).to.not.be.an('undefined');
-
-      const constructorParams = app.constructorArgs;
-      const config = constructorParams.config;
-      cy.wait(2000);
-
-      cy.get('.app-header__title').should(($div) => {
-        const titleText = $div.get(0).innerText;
-        expect(titleText.toLowerCase()).to.eq(config.title.toLowerCase());
-      });
-      cy.get('.app-header__subtitle').should(($div) => {
-        const subtitleText = $div.get(0).innerText;
-        expect(subtitleText.toLowerCase()).to.eq(config.subtitle.toLowerCase());
-      });
-
-      cy.get('.app-header__nav-list').children().should('have.length', 1);
-
-      cy.get('.app-header__logo-container').find('img').should('have.attr', 'src').should('include', config.logoUrl);
-      cy.get('.app-header__logo-container').find('a').should('have.attr', 'href').should('include', config.logoLinkUrl);
-
-      cy.get('.app-header__nav-link--map-themes').should('have.length', 0);
-      cy.get('.app-header__nav-link--gfw-login').should('have.length', 1);
-      cy.get('.app-header__nav-link--language').should('have.length', 0);
-
-      if (config.includeMeasurementTab) {
-        cy.get('[data-value="MEASUREMENT"]').should('have.length', 1);
-      } else {
-        cy.get('[data-value="MEASUREMENT"]').should('have.length', 0);
-      }
-
-      if (config.includeDocumentsTab) {
-        cy.get('[data-value="DOCUMENTS"]').should('have.length', 1);
-      } else {
-        cy.get('[data-value="DOCUMENTS"]').should('have.length', 0);
-      }
-
-      if (config.narrative) {
-        cy.get('[data-value="NARRATIVE"]').should('have.length', 1);
-      } else {
-        cy.get('[data-value="NARRATIVE"]').should('have.length', 0);
-      }
+      const ll = cy.get('.layer-category-content').not('.closed');
+      console.log('ll', ll);
 
     });
 
   });
+
+  // it("Tests our site's html for our constructor parameters", function () {
+  //
+  //   cy.visit('https://my.gfw-mapbuilder.org/v1.4.0/');
+  //
+  //   cy.window().then(windowObject => {
+  //     const app = windowObject.customApp;
+  //     expect(app).to.not.be.an('undefined');
+  //
+  //     const constructorParams = app.constructorArgs;
+  //     const config = constructorParams.config;
+  //     cy.wait(2000);
+  //
+  //     cy.get('.app-header__title').should(($div) => {
+  //       const titleText = $div.get(0).innerText;
+  //       expect(titleText.toLowerCase()).to.eq(config.title.toLowerCase());
+  //     });
+  //     cy.get('.app-header__subtitle').should(($div) => {
+  //       const subtitleText = $div.get(0).innerText;
+  //       expect(subtitleText.toLowerCase()).to.eq(config.subtitle.toLowerCase());
+  //     });
+  //
+  //     cy.get('.app-header__nav-list').children().should('have.length', 1);
+  //
+  //     cy.get('.app-header__logo-container').find('img').should('have.attr', 'src').should('include', config.logoUrl);
+  //     cy.get('.app-header__logo-container').find('a').should('have.attr', 'href').should('include', config.logoLinkUrl);
+  //
+  //     cy.get('.app-header__nav-link--map-themes').should('have.length', 0);
+  //     cy.get('.app-header__nav-link--gfw-login').should('have.length', 1);
+  //     cy.get('.app-header__nav-link--language').should('have.length', 0);
+  //
+  //     if (config.includeMeasurementTab) {
+  //       cy.get('[data-value="MEASUREMENT"]').should('have.length', 1);
+  //     } else {
+  //       cy.get('[data-value="MEASUREMENT"]').should('have.length', 0);
+  //     }
+  //
+  //     if (config.includeDocumentsTab) {
+  //       cy.get('[data-value="DOCUMENTS"]').should('have.length', 1);
+  //     } else {
+  //       cy.get('[data-value="DOCUMENTS"]').should('have.length', 0);
+  //     }
+  //
+  //     if (config.narrative) {
+  //       cy.get('[data-value="NARRATIVE"]').should('have.length', 1);
+  //     } else {
+  //       cy.get('[data-value="NARRATIVE"]').should('have.length', 0);
+  //     }
+  //
+  //   });
+  //
+  // });
 
 });
--- src/js/utils/template.js
+++ src/js/utils/template.js
@@ -291,8 +291,12 @@ export default {
   * @return {promise} promise
   */
   getAppInfo: (id, constructorParams) => {
+    console.log('getAppInfo', id);
     const promise = new Deferred();
     const appid = id ? id : getUrlParams(location.href).appid;
+
+    console.log('appid', appid);
+    console.log('constructorParams', constructorParams);
 
     // Set the sharinghost to the correct location so the app can find the webmap content
     if (!resources.sharinghost) { resources.sharinghost = 'https://www.arcgis.com'; }
@@ -307,13 +311,17 @@ export default {
         //- This will merge all the settings in
         lang.mixin(resources, constructorParams);
       }
+      console.log(' new constructorParams', constructorParams);
 
       //- Format the resources before resolving
       formatResources().then(formattedResources => {
+        console.log('formattedResources', formattedResources);
         promise.resolve(formattedResources);
       });
       return promise;
     }
+    console.log('we here??');
+    // debugger
 
     arcgisUtils.getItem(appid).then(res => {
       let agolValues = res.itemData && res.itemData.values;
@@ -330,6 +338,7 @@ export default {
       if (!agolValues) {
         //- Format the resources before resolving
         formatResources().then(formattedResources => {
+          console.log('formattedResources2 ', formattedResources);
           promise.resolve(formattedResources);
         });
         return promise;
@@ -345,6 +354,7 @@ export default {
 
         //- Format the resources before resolving
         formatResources().then(formattedResources => {
+          console.log('formattedResources3 ', formattedResources);
           promise.resolve(formattedResources);
         });
 
@@ -353,6 +363,7 @@ export default {
     }, err => {
       if (brApp.debug) { console.warn(`template.getAppInfo >> ${err.message}`); }
       formatResources().then(formattedResources => {
+        console.log('formattedResources4', formattedResources);
         promise.resolve(formattedResources);
       });
     });
