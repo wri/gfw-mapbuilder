a227b1e9e571447b2362b78e3ab432ab0286a442
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-02 15:44:29 -0400

    Remove unneeded comments in ReportSubscribe

--- src/js/components/Shared/ReportSubscribe.js
+++ src/js/components/Shared/ReportSubscribe.js
@@ -30,10 +30,6 @@ export default class ReportSubscribeButtons extends Component {
   printReport = () => {
     const { map, settings, language } = this.context;
     const selectedFeature = map.infoWindow && map.infoWindow.getSelectedFeature();
-    //const selectedFeatureTitle = selectedFeature._layer.infoTemplate.title(selectedFeature);
-    //This will create an array of the titles when we add in functionality for more than one feature to be displayed in the analysis area in the report
-    // const selectedFeatureObj = map.infoWindow.getSelectedFeature();
-    // const selectedFeatureTitles = selectedFeatureObj.attributes.title.split(',');
     const {
       canopyDensity,
       activeSlopeClass,
