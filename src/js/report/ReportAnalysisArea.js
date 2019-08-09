28f70e5188617286927070a06ddedc9820734d27
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-09 10:28:54 -0400

    Add extra check in ReportAnalysisArea for selectedFeatureTitles; fix resources and external

--- src/js/report/ReportAnalysisArea.js
+++ src/js/report/ReportAnalysisArea.js
@@ -5,14 +5,17 @@ export default class ReportAnalysisArea extends Component {
 
     render() {
         const {selectedFeatureTitles} = this.props;
-        const selectedFeatureTitlesArray = selectedFeatureTitles.split(',');
+        let selectedFeatureTitlesArray;
+        if (selectedFeatureTitles !== ''){
+            selectedFeatureTitlesArray = selectedFeatureTitles.split(',');
+        }
         return (
             <div className="map-analysis-area-container">
                 <div id="map" className="map"></div>
                 <div id="analysis-area" className="analysis-area">
                     <span className="analysis-area-subtitle">AREA OF ANALYSIS</span>
                     <ul className="analysis-area-list">
-                        {selectedFeatureTitlesArray.length > 0 && selectedFeatureTitlesArray.map((title, index) => <li key={`analysis-area-${index}`}className="analysis-area-list-item">{title}</li>)}
+                        {selectedFeatureTitlesArray && selectedFeatureTitlesArray.length > 0 && selectedFeatureTitlesArray.map((title, index) => <li key={`analysis-area-${index}`}className="analysis-area-list-item">{title}</li>)}
                     </ul>
                 </div>
             </div>
