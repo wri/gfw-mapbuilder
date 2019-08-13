9a464bac17cae75eb7ab91d07be52e6d67be444a
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-13 17:35:23 -0400

    WIP - build out modal content

--- src/js/components/Modals/CoordinatesModal.js
+++ src/js/components/Modals/CoordinatesModal.js
@@ -4,14 +4,59 @@ import React, { Component } from 'react';
 
 export default class AnalysisModal extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      DMS: true,
+      DD: false
+    };
+  }
+
   close = () => {
     mapActions.toggleCoordinatesModal({ visible: false });
+  };
+  
+  handleChange = () => {
+    this.setState({
+      DMS: !this.state.DMS,
+      DD: !this.state.DD
+    });
+  };
+  
+  createOptions = (analysisObj) => {
+    const { language } = this.context;
+    const { analysisId, label } = analysisObj;
+    return (
+      <option
+        key={analysisId}
+        value={analysisId}
+      >
+        {label[language] ? label[language] : ''}
+      </option>
+    );
   };
 
   render () {
     return (
       <ControlledModalWrapper onClose={this.close}>
-        <div>TEST</div>
+        <div className='relative analysis-results__select-container'>
+          <select
+            value={activeAnalysisType || 'default'}
+            className='analysis-results__select pointer'
+            onChange={this.handleChange}
+          >
+            {analysisItems.map(this.createOptions)}
+          </select>
+          <div className='analysis-results__select-arrow' />
+        </div>
+         <div>
+          <label htmlFor="deg-lat">Latitude</label>
+          <input ref='decimalDegreeLat' type='number' id='deg-lat' name='deg-lat' />
+        </div>
+        <div>
+        <label htmlFor="deg-lng">Longitude</label>
+          <input ref='decimalDegreeLng' type='number' id='deg-lng' name='deg-lng' />
+        </div>
       </ControlledModalWrapper>
     );
   }
