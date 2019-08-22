6a9a1c32ed43a5579490841fa30133097846e9fb
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-22 15:26:24 -0400

    Only display the first item in the selectedFeatureTitlesArray

--- src/js/report/ReportAnalysisArea.js
+++ src/js/report/ReportAnalysisArea.js
@@ -14,9 +14,14 @@ export default class ReportAnalysisArea extends Component {
                 <div id="map" className="map"></div>
                 <div id="analysis-area" className="analysis-area">
                     <span className="analysis-area-subtitle">AREA OF ANALYSIS</span>
-                    <ul className="analysis-area-list">
+                    {/* <ul className="analysis-area-list">
                         {selectedFeatureTitlesArray && selectedFeatureTitlesArray.length > 0 && selectedFeatureTitlesArray.map((title, index) => <li key={`analysis-area-${index}`}className="analysis-area-list-item">{title}</li>)}
-                    </ul>
+                    </ul> */}
+                    {selectedFeatureTitlesArray && selectedFeatureTitlesArray.length > 0 &&
+                        <div className="analysis-area-selected-feature-title">
+                            {selectedFeatureTitlesArray[0]}
+                        </div>
+                    }
                 </div>
             </div>
         );
