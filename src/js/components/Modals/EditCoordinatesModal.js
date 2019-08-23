a75f6f080780d7f58b9047ff3e205ece45f2b65f
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-23 15:35:44 -0400

    Continue setting up component

--- src/js/components/Modals/EditCoordinatesModal.js
+++ src/js/components/Modals/EditCoordinatesModal.js
@@ -31,10 +31,7 @@ export default class EditCoordinatesModal extends Component {
       ddCoordinates.push(defaultDD);
     }
     this.setState({
-      coordinatesFormat: '',
-      dmsCoordinates,
-      ddCoordinates,
-      errors: []
+      coordinateValue: ''
     });
   };
 
@@ -52,44 +49,34 @@ export default class EditCoordinatesModal extends Component {
       errors: []
     });
   };
+  
+  editCoordinate = evt => {
+    console.log('edit coordinate!');
+  };
 
   render () {
     const {language} = this.context;
-    const {coordinatesFormat, dmsCoordinates, ddCoordinates, errors} = this.state;
-    const coordinateFormatOptions = text[language].ANALYSIS_COORDINATES_FORMATS;
+    const {coordinateValue} = this.state;
     
     return (
       <ControlledModalWrapper onClose={this.close}>
-        <h4 className="analysis-instructions__header">{text[language].ANALYSIS_COORDINATES_HEADER}</h4>
-        <div className='relative analysis-coordinates__select-container'>
-          <label htmlFor="coordinates-formats" className="analysis-coordinates__label">{text[language].ANALYSIS_COORDINATES_LABELS[2]}</label>
-          <select
-            value={coordinatesFormat ? coordinatesFormat : coordinateFormatOptions[0]}
-            className='analysis-coordinates__select pointer'
-            onChange={evt => this.switchCoordinatesFormat(evt)}
-            id="coordinates-formats"
-          >
-            {coordinateFormatOptions && coordinateFormatOptions.map(this.createOptions)}
-          </select>
-          <div className='analysis-coordinates__select-arrow'></div>
+        <div className="edit-coordinate-header">
+          <h4 className="edit-coordinate-title">{text[language].EDIT_COORDINATE_LABELS[0]}</h4>
+          <div className="edit-coordinate-delete">
+            <span className="edit-coordinate-delete-text">{text[language].EDIT_COORDINATE_LABELS[1]}</span>
+            <SVGIcon id={'icon-analysis-remove'} />
+          </div>
         </div>
-
-        {coordinatesFormat === coordinateFormatOptions[1] && <div className="analysis-coordinates__divider-dd"></div>}
-        {(coordinatesFormat === coordinateFormatOptions[0] || coordinatesFormat === '') &&
-        dmsCoordinates.map((item, index) => this.renderDMS(item, index))}
-        {coordinatesFormat === coordinateFormatOptions[1] &&
-        ddCoordinates.map((item, index) => this.renderDD(item, index))}
-        {coordinatesFormat === coordinateFormatOptions[1] && <div className="analysis-coordinates__divider-dd"></div>}
-
-        <div className="fa-button analysis-instructions__add-more-button" onClick={this.addMore}>
-          <span className="analysis-instructions__add-more-icon"><SVGIcon id={'icon-add-more'} /></span>
-          <span className="analysis-instructions__add-more">{text[language].ANALYSIS_COORDINATES_BUTTONS[1]}</span>
+        <div className='edit-coordinate-input-container'>
+          <input
+            onChange={evt => this.editCoordinate(evt)}
+            className="edit-coordinate-input"
+            type='number'
+            id="edit-coordinate"
+            name="edit-coordinate"
+            value={coordinateValue}
+          />
         </div>
-        <div className="fa-button gold analysis-instructions__make-shape-button" onClick={this.validateShape}>
-          {/* <span className="analysis-instructions__make-shape-icon"><SVGIcon id={'icon-shape'} /></span> */}
-          <span className="analysis-instructions__make-shape">{text[language].ANALYSIS_COORDINATES_BUTTONS[2]}</span>
-        </div>
-        {errors !== [] && <div className="analysis-coordinates-errors">{errors.map(error => <div className="analysis-coordinates-error"> {text[language].ANALYSIS_COORDINATES_ERROR[error]}</div>)}</div>}
       </ControlledModalWrapper>
     );
   }
