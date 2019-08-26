519e18b52c7ff3164bc78d7769e964197025263c
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-26 17:08:19 -0400

    Get appropriate DMS or DD inputs to show up in EditCoordinateModal

--- src/js/components/Modals/EditCoordinatesModal.js
+++ src/js/components/Modals/EditCoordinatesModal.js
@@ -45,6 +45,156 @@ export default class editCoordinatessModal extends Component {
   editCoordinates = evt => {
     console.log('value', evt.target.value);
   };
+  
+  renderDMS = (item, index) => {
+    const {language} = this.context;
+    const latitudeDirectionOptions = text[language].ANALYSIS_COORDINATES_LATITUDE_DIRECTIONS;
+    const longitudeDirectionOptions = text[language].ANALYSIS_COORDINATES_LONGITUDE_DIRECTIONS;
+    
+    return (
+      <div key={`DMS-${index}`}>
+      {index === 0 && <div className="analysis-coordinates__divider-dms"></div>}
+      <div className="analysis-coordinates__inputs-container-dms">
+        {index > 2 &&
+          <div className="analysis-coordinates-remove" onClick={() => this.remove(index)}>
+            <span className="analysis-coordinates-remove-text">{text[language].ANALYSIS_COORDINATES_BUTTONS[3]}</span>
+            <SVGIcon id={'icon-analysis-remove'} />
+          </div>
+        }
+        <div className="analysis-coordinates__latitude-container">
+          <span className="analysis-coordinates__latitude-label">{text[language].ANALYSIS_COORDINATES_LABELS[0]}</span>
+          <div className="analysis-coordinates__latitude">
+            <input
+              onChange={evt => this.updateDMS(evt, index, 'degrees', 'lat')}
+              className="analysis-coordinates__latitude-measurement"
+              type='number'
+              id={`dms-latitude-degrees-${item}`}
+              name={`dms-latitude-${item}`}
+              value={item.lat.degrees}
+            />
+            <span className="analysis-coordinates__latitude-measurement-label">&deg;</span>
+            <input
+              onChange={evt => this.updateDMS(evt, index, 'minutes', 'lat')}
+              className="analysis-coordinates__latitude-measurement"
+              type='number'
+              id={`dms-latitude-minutes-${item}`}
+              name={`dms-latitude-${item}`}
+              value={item.lat.minutes}
+            />
+            <span className="analysis-coordinates__latitude-measurement-label">'</span>
+            <input
+              onChange={evt => this.updateDMS(evt, index, 'seconds', 'lat')}
+              className="analysis-coordinates__latitude-measurement"
+              type='number'
+              id={`dms-latitude-seconds-${item}`}
+              name={`dms-latitude-${item}`}
+              value={item.lat.seconds}
+            />
+            {/* <span className="analysis-coordinates__latitude-measurement-label">"</span>
+            <select
+              className='analysis-coordinates-directions__select pointer'
+              onChange={evt => this.updateDMS(evt, index, 'direction', 'lat')}
+              id={`dms-latitude-direction-${item}`}
+              name={`dms-latitude-${item}`}
+              value={item.lat.direction}
+            >
+              {latitudeDirectionOptions && latitudeDirectionOptions.map(this.createOptions)}
+            </select> */}
+            <div className='analysis-coordinates-directions__select-arrow'></div>
+          </div>
+        </div>
+        
+        <div className="analysis-coordinates__longitude-container">
+          <span className="analysis-coordinates__longitude-label">{text[language].ANALYSIS_COORDINATES_LABELS[1]}</span>
+          <div className="analysis-coordinates__longitude">
+            <input
+              onChange={evt => this.updateDMS(evt, index, 'degrees', 'lng')}
+              className="analysis-coordinates__longitude-measurement"
+              type='number'
+              id={`dms-longitude-degrees-${item}`}
+              name={`dms-longitude-${item}`}
+              value={item.lng.degrees}
+            />
+            <span className="analysis-coordinates__longitude-measurement-label">&deg;</span>
+            <input
+              onChange={evt => this.updateDMS(evt, index, 'minutes', 'lng')}
+              className="analysis-coordinates__longitude-measurement"
+              type='number'
+              id={`dms-longitude-minutes-${item}`}
+              name={`dms-longitude-${item}`}
+              value={item.lng.minutes}
+            />
+            <span className="analysis-coordinates__longitude-measurement-label">'</span>
+            <input
+              onChange={evt => this.updateDMS(evt, index, 'seconds', 'lng')}
+              className="analysis-coordinates__longitude-measurement"
+              type='number' id={`dms-longitude-seconds-${item}`}
+              name={`dms-longitude-${item}`}
+              value={item.lng.seconds}
+            />
+            {/* <span className="analysis-coordinates__longitude-measurement-label">"</span>
+            <select
+              className='analysis-coordinates-directions__select pointer'
+              onChange={evt => this.updateDMS(evt, index, 'direction', 'lng')}
+              id={`dms-longitude-direction-${item}`}
+              name={`dms-longitude-${item}`}
+              value={item.lng.direction}
+            >
+              {longitudeDirectionOptions && longitudeDirectionOptions.map(this.createOptions)}
+            </select> */}
+            <div className='analysis-coordinates-directions__select-arrow'></div>
+          </div>
+        </div>
+      </div>
+      <div className="analysis-coordinates__divider-dms"></div>
+      </div>
+    );
+  };
+  
+  renderDD = (item, index) => {
+    const {language} = this.context;
+    return (
+      <div key={`DD-${index}`} className="analysis-coordinates__inputs-container-dd">
+        {index > 0 && <div className="analysis-coordinates__divider-dd"></div>}
+        {index > 2 &&
+          <div className="analysis-coordinates-remove" onClick={() => this.remove(index)}>
+            <span className="analysis-coordinates-remove-text">{text[language].ANALYSIS_COORDINATES_BUTTONS[3]}</span>
+            <SVGIcon id={'icon-analysis-remove'} />
+          </div>
+        }
+        <div className="analysis-coordinates-lat-lng-container">
+        <div className="analysis-coordinates__latitude-container">
+          <span className="analysis-coordinates__latitude-label">{text[language].ANALYSIS_COORDINATES_LABELS[0]}</span>
+          <div className="analysis-coordinates__latitude">
+            <input
+              onChange={evt => this.updateDD(evt, index, 'lat')}
+              className="analysis-coordinates__latitude-measurement"
+              type='number' id={`dd-latitude-degrees-${item}`}
+              name={`dd-${item}`}
+              value={item.lat}
+            />
+            <span className="analysis-coordinates__latitude-measurement-label">&deg;</span>
+          </div>
+        </div>
+        
+        <div className="analysis-coordinates__longitude-container">
+          <span className="analysis-coordinates__longitude-label">{text[language].ANALYSIS_COORDINATES_LABELS[1]}</span>
+          <div className="analysis-coordinates__longitude">
+            <input
+              onChange={evt => this.updateDD(evt, index, 'lng')}
+              className="analysis-coordinates__longitude-measurement"
+              type='number'
+              id={`dd-longitude-degrees-${item}`}
+              name={`dd-${item}`}
+              value={item.lng}
+            />
+            <span className="analysis-coordinates__longitude-measurement-label">&deg;</span>
+          </div>
+        </div>
+        </div>
+    </div>
+    );
+  };
 
   render () {
     const {language} = this.context;
@@ -61,7 +211,7 @@ export default class editCoordinatessModal extends Component {
               ${coordinatesFormat === text[language].ANALYSIS_COORDINATES_FORMATS[0] ? text[language].EDIT_COORDINATES_FORMATS[0] : text[language].EDIT_COORDINATES_FORMATS[1]  }`
             }
           </h4>
-          <div className="edit-coordinates-delete">
+          {/* <div className="edit-coordinates-delete">
             <span className="edit-coordinates-delete-text">{text[language].EDIT_COORDINATES_LABELS[1]}</span>
           </div>
         </div>
@@ -73,7 +223,12 @@ export default class editCoordinatessModal extends Component {
             id="edit-coordinates"
             name="edit-coordinates"
             value={coordinateValue}
-          />
+          /> */}
+          {
+            coordinatesFormat === text[language].ANALYSIS_COORDINATES_FORMATS[0] ?
+            dmsCoordinates.map((item, index) => this.renderDMS(item, index)) :
+            ddCoordinates.map((item, index) => this.renderDD(item, index))
+          }
         </div>
       </ControlledEditModalWrapper>
     );
