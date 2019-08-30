970914d22aa59420291562ee3d893b0f01d84c1f
Kayla Kremer <kremer.kayla@gmail.com>
2019-08-30 12:26:44 -0400

    WIP

--- src/js/stores/MapStore.js
+++ src/js/stores/MapStore.js
@@ -356,7 +356,7 @@ class MapStore {
           analysisUtils.getExactGeom(selectedFeature).then(exactGeom => {
             //If the geometry we got back from the server is in the wrong spatialRef, let's just use the original geometry!
             const geomToRegister = exactGeom.spatialReference.isWebMercator() ? exactGeom : selectedFeature.geometry;
-            analysisUtils.registerGeom(exactGeom).then(res => {
+            analysisUtils.registerGeom(geomToRegister).then(res => {
               selectedFeature.attributes.geostoreId = res.data.id;
               selectedFeature.setGeometry(geomToRegister);
               mapActions.toggleAnalysisTab(false);
