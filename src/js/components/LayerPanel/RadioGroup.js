4fbeb3e5637bd70a8a3c9f012e7bf66c8a3e9480
Kayla Kremer <kremer.kayla@gmail.com>
2019-09-10 16:19:51 -0400

    Make radio button groups mutually exclusive when selecting option from each group

--- src/js/components/LayerPanel/RadioGroup.js
+++ src/js/components/LayerPanel/RadioGroup.js
@@ -24,10 +24,6 @@ import resources from './../../../resources';
     const layerPanelGroups = Object.keys(layerPanel);
     let matchedGroup;
     
-    console.log('layerPanel', resources.layerPanel);
-    console.log('activeLayers', activeLayers);
-    console.log('layer', layer);
-    
     layerPanelGroups.forEach(group => {
       if (layerPanel[group].layers && layerPanel[group].layers.length > 0) {
         layerPanel[group].layers.forEach(groupLayer => {
@@ -37,59 +33,29 @@ import resources from './../../../resources';
         });
       }
     });
-    
-    console.log('matchedGroup', matchedGroup);
 
-    // if (activeLayers.includes(groupID)) {
-    //   const index = activeLayers.indexOf(groupID);
-    //   activeLayers.splice(index, 1);
-    // }
-
-    //const layersAlreadyHidden = [];
-    //allRadioLayers.forEach(l => {
-      //if (l.id === layer.id) {
-        //if (l.subId || l.layerIds) { layerActions.removeAllSubLayers(l.esriLayer); }
-        //return;
-      //}
-      // if (layersAlreadyHidden.indexOf(l.id) > -1) {
-      //   return;
-      // }
-
-      //layerActions.removeActiveLayer(l.subId || l.id);
-      //if (l.subId || l.layerIds) { layerActions.removeAllSubLayers(l.esriLayer); }
-      //layersAlreadyHidden.push(l.id);
-    //});
-
-    //If the item you clicked is already on
-   // turn it off and return from the function.
-   
+    // If the item you clicked is already on
+    // turn it off and return from the function.
     if (activeLayers.indexOf(layer.subId || layer.id) > -1) {
-      console.log('1');
       layerActions.removeActiveLayer(layer.subId || layer.id);
       if (layer.subId || layer.layerIds) { layerActions.removeAllSubLayers(layer.esriLayer); }
-      return;
     }
 
     if (layer.hasOwnProperty('subId')) {
-      console.log('2');
       layerActions.setSubLayers(layer, layer.subIndex);
       layer.esriLayer.setVisibleLayers([layer.subIndex]);
-      return;
     }
 
     if (layer.hasOwnProperty('layerIds')) {
-      console.log('3');
       layerActions.setSubLayers(layer, ...layer.layerIds);
       layer.esriLayer.setVisibleLayers(layer.layerIds);
-      return;
     }
     
-    console.log('4');
     layerActions.addActiveLayer(layer.id);
     
     if (layerPanel[matchedGroup].layers && layerPanel[matchedGroup].layers.length > 0) {
       layerPanel[matchedGroup].layers.forEach(groupLayer => {
-        if (activeLayers.includes(groupLayer.id) && activeLayers.includes(layer.id)) {
+        if (activeLayers.includes(groupLayer.id)) {
           layerActions.removeActiveLayer(groupLayer.id);
         }
       });
