64ec579505bddda2c3f9435f03a70a41d15de032
Lucas Cotner <lc07@uw.edu>
2019-09-17 09:27:47 -0400

    Allowing library to build domNodes it attaches itself to

--- src/js/reportLibraryMain.js
+++ src/js/reportLibraryMain.js
@@ -9,22 +9,12 @@ import ShareModal from './components/Modals/ShareModal';
 
 // if (!_babelPolyfill) { console.log('Missing Babel Polyfill.  May experience some weirdness in IE < 9.'); }
 
-// window.brApp = {
-//   debugEnabled: true,
-//   debug: function (message) {
-//     if (this.debugEnabled) {
-//       var print = typeof message === 'string' ? console.log : console.dir;
-//       print.apply(console, [message]);
-//     }
-//   }
-// };
 // corsServers.forEach((server) => { esriConfig.defaults.io.corsEnabledServers.push(server); });
 
-
-    loadCSS(window._app.base + '/css/report.css');
-    loadCSS('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,300');
-    loadCSS('https://js.arcgis.com/3.17/dijit/themes/tundra/tundra.css');
-    loadCSS('https://js.arcgis.com/3.17/esri/css/esri.css');
+  loadCSS(window._app.base + '/css/report.css');
+  loadCSS('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,300');
+  loadCSS('https://js.arcgis.com/3.17/dijit/themes/tundra/tundra.css');
+  loadCSS('https://js.arcgis.com/3.17/esri/css/esri.css');
 
   export default class ReportLibraryMain extends Component {
     constructor(props) {
@@ -50,15 +40,20 @@ import ShareModal from './components/Modals/ShareModal';
   }
 
   let reportNode = document.getElementById('report');
+  let shareNode = document.getElementById('share-modal');
+
   if (!reportNode) {
     const reportNodes = document.getElementsByClassName('report');
     reportNode = reportNodes.item(0);
   }
 
-  const shareNode = document.getElementById('share-modal');
+  if (!shareNode) {
+    shareNode = document.createElement('div');
+    shareNode.setAttribute('id', 'share-modal');
+    shareNode.classList.add('share-modal');
+    shareNode.classList.add('hidden');
+    reportNode.parentNode.insertBefore(shareNode, reportNode.nextSibling);
+  }
 
   ReactDOM.render(<ReportLibraryMain />, reportNode);
-
-  if (shareNode) {
-    ReactDOM.render(<ShareModal url={window.location.href} />, shareNode);
-  }
+  ReactDOM.render(<ShareModal url={window.location.href} />, shareNode);
