991a371870c5b156af7c51e3700ea023ba431657
Kayla Kremer <kremer.kayla@gmail.com>
2019-10-24 09:46:56 -0400

    WIP

--- src/js/report/ReportTable.js
+++ src/js/report/ReportTable.js
@@ -3,6 +3,7 @@ import QueryTask from 'esri/tasks/QueryTask';
 import Query from 'esri/tasks/query';
 import locale from 'dojo/date/locale';
 import number from 'dojo/number';
+import lang from 'dojo/_base/lang';
 
 export default class ReportTable extends Component {
     constructor(props){
@@ -22,46 +23,53 @@ export default class ReportTable extends Component {
           console.log('url', url);
           const id = hashDecoupled[1];
           const mapLayer = map.getLayer(id);
-          console.log('mapLayer', mapLayer);
-          const queryTask = new QueryTask(mapLayer.url);
+          const queryTask = new QueryTask(url);
           const query = new Query();
           query.where = OBJECTID_Field + ' = ' + OBJECTID;
           query.returnGeometry = false;
           query.outFields = ['*'];
-          queryTask.execute(query).then(res => {
-            console.log('res', res);
-            if (res.features && res.features.length > 0) {
-              if (mapLayer && mapLayer.infoTemplate) {
-                const tableFields = [];
-                mapLayer.infoTemplate.info.fieldInfos.filter(fieldInfo => fieldInfo.visible).forEach((fieldInfo) => {
-                  let fieldValue = res.features[0].attributes[fieldInfo.fieldName];
-                  console.log('fieldValue', fieldValue);
-                  //- If it is a date, format that correctly
-                  if (fieldInfo.format && fieldInfo.format.dateFormat) {
-                    fieldValue = locale.format(new Date(fieldValue));
-                  //- If it is a number, format that here, may need a better way
-                  } else if (fieldInfo.format && fieldInfo.format.places !== undefined) {
-                    fieldValue = number.format(fieldValue, fieldInfo.format);
-                  }
-                  if (fieldValue && fieldValue.trim) {
-                    fieldValue = fieldValue.trim();
-                    tableFields.push({
-                      fieldLabel: fieldInfo.label,
-                      fieldValue
-                    });
-                  }
-                });
-                this.setState({
-                  tableFields
+          // queryTask.execute(query, lang.hitch(this, function(results) {
+          //   console.log(results.features);
+          //   }));
+          queryTask.execute(query, this.queryCallback);
+        }
+      };
+      
+      queryCallback = res => {
+        console.log('res', res);
+        const {map, params} = this.props;
+        const { layerId } = params;
+        const hashDecoupled = layerId.split('--');
+        const id = hashDecoupled[1];
+        const mapLayer = map.getLayer(id);
+        if (res.features && res.features.length > 0) {
+          if (mapLayer && mapLayer.infoTemplate) {
+            const tableFields = [];
+            mapLayer.infoTemplate.info.fieldInfos.filter(fieldInfo => fieldInfo.visible).forEach((fieldInfo) => {
+              let fieldValue = res.features[0].attributes[fieldInfo.fieldName];
+              console.log('fieldValue', fieldValue);
+              //- If it is a date, format that correctly
+              if (fieldInfo.format && fieldInfo.format.dateFormat) {
+                fieldValue = locale.format(new Date(fieldValue));
+              //- If it is a number, format that here, may need a better way
+              } else if (fieldInfo.format && fieldInfo.format.places !== undefined) {
+                fieldValue = number.format(fieldValue, fieldInfo.format);
+              }
+              if (fieldValue && fieldValue.trim) {
+                fieldValue = fieldValue.trim();
+                tableFields.push({
+                  fieldLabel: fieldInfo.label,
+                  fieldValue
                 });
               }
-            }
-          }).catch(err => {
-            console.log('error:', err);
-          });
+            });
+            this.setState({
+              tableFields
+            });
+          }
         }
-    };
-    
+      };
+
     componentDidMount() {
       this.addTableAttributes();
     }
