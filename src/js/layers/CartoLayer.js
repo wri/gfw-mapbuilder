0298847ac0fa8a195e2d7c470c1827681657f276
Vaidotas Piekus <vaidotasp@gmail.com>
2020-01-07 16:57:39 -0500

    deleting files

--- src/js/layers/CartoLayer.js
+++
@@ -1,9 +0,0 @@
-import CartoLayerBase from './CartoLayerBase';
-import declare from 'dojo/_base/declare';
-
-export default declare('CartoLayer', [CartoLayerBase], {
-  queryBuilder: function() {
-    // this.getLayers();
-    this.init();
-  }
-});
--- src/locals.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  author: 'WRI, BlueRaster',
-  esriVersion: '3.22',
-  defaultTitle: 'GFW Mapbuilder',
-  base: ''
-};
