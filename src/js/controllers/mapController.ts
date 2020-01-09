8be21e80f199804c893707fbfcf5b24512a05437
Vaidotas Piekus <vaidotasp@gmail.com>
2020-01-09 11:06:04 -0500

    dojo config loader import rename and fix

--- src/js/controllers/mapController.ts
+++ src/js/controllers/mapController.ts
@@ -1,6 +1,7 @@
-import Map from "esri/Map";
-import MapView from "esri/views/MapView";
-import { RefObject } from "react";
+import Map from 'esri/Map';
+import MapView from 'esri/views/MapView';
+import WebMap from 'esri/WebMap';
+import { RefObject } from 'react';
 
 export class MapController {
   _map: Map | null;
@@ -12,19 +13,19 @@ export class MapController {
   }
 
   initializeMap(domRef: RefObject<any>) {
-    console.log(domRef);
-    this._map = new Map({ basemap: "topo" });
-
+    this._map = new WebMap({
+      portalItem: {
+        id: 'e691172598f04ea8881cd2a4adaa45ba'
+      }
+    });
     this._mapview = new MapView({
       map: this._map,
-      container: domRef.current,
-      center: [-77.091, 38.8816],
-      zoom: 12
+      container: domRef.current
     });
 
     this._mapview.when(
       () => {
-        console.log("mapview is loaded");
+        console.log('mapview is loaded');
       },
       (error: Error) => {
         console.log(error);
@@ -32,8 +33,8 @@ export class MapController {
     );
   }
 
-  log() {
-    console.log(this._map?.basemap.id);
+  log(): void {
+    console.log(this._map?.basemap);
   }
 }
 
