f474162a371d6ff8c22ed6087c7cf566d158c117
Eden <eden.s.mesfun@gmail.com>
2020-01-14 17:07:44 -0500

    header is integrated with flexbox and is mobile-responsive

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
-import Mapview from './mapview/Mapview';
+// import Mapview from './mapview/Mapview';
 import Header from './header/Header';
-import { RootState } from 'js/store/index';
-import { useSelector } from 'react-redux';
+// import { RootState } from 'js/store/index';
+// import { useSelector } from 'react-redux';
 
 import 'arcgis-js-api/themes/light/main.scss';
 import 'css/index.scss';
 
-const Loader = (): React.ReactElement => <h4>Map Loading...</h4>;
+// const Loader = (): React.ReactElement => <h4>Map Loading...</h4>;
 
-const ErrorScreen = (): React.ReactElement => <h4>Map Loading Error</h4>;
+// const ErrorScreen = (): React.ReactElement => <h4>Map Loading Error</h4>;
 
 const App = (): JSX.Element => {
-  const isMapReady = useSelector(
-    (store: RootState) => store.mapviewState.isMapReady
-  );
-  const loadError = useSelector(
-    (store: RootState) => store.mapviewState.loadError
-  );
+  // const isMapReady = useSelector(
+  //   (store: RootState) => store.mapviewState.isMapReady
+  // );
+  // const loadError = useSelector(
+  //   (store: RootState) => store.mapviewState.loadError
+  // );
 
   return (
     <>
       <Header />
-      <Mapview />
-      {!isMapReady && <Loader />}
-      {loadError && <ErrorScreen />}
+      {/* <Mapview /> */}
+      {/* {!isMapReady && <Loader />}
+      {loadError && <ErrorScreen />} */}
     </>
   );
 };
