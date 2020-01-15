4da3c602cf784f0f5c3cec68db1711c47248008e
Eden <eden.s.mesfun@gmail.com>
2020-01-15 17:03:57 -0500

    integrated eslint accessibility module

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,14 @@ module.exports = {
     node: true
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'jsx-a11y'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
   rules: {
     semi: 'warn',
