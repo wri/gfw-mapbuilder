f3f4f67eae742a7df7877721e5a2af7fcdef161d
Eden <eden.s.mesfun@gmail.com>
2020-01-24 17:53:10 -0500

    refactored to remove repetitive code. Uses row constant to generate number of rows for DMS

--- src/js/components/mapWidgets/widgetContent/coordinatesForm.tsx
+++ src/js/components/mapWidgets/widgetContent/coordinatesForm.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 
 import { coordinatesContent } from '../../../../../configs/modal.config';
@@ -20,169 +20,127 @@ const CoordinatesForm: FunctionComponent = () => {
     longitude
   } = coordinatesContent[selectedLanguage];
 
-  const returnSelectedContent = () => {
+  const latitudeLongitudeRow = (): any => {
     if (decimalOptions[selectedFormat].includes('DMS')) {
-      return (
-        <>
-          <div className="dms-wrapper">
-            <span>{latitude}</span>
-            <div className="input-wrapper">
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{degree}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{apostrophe}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{quotes}</span>
-              <select onChange={e => console.log('hmmm', e.target.value)}>
-                <option value="N">N</option>
-                <option value="S">S</option>
-              </select>
-            </div>
-          </div>
-          <div className="dms-wrapper">
-            <span>{longitude}</span>
-            <div className="input-wrapper">
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{degree}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{apostrophe}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{quotes}</span>
-              <select onChange={e => console.log('hmmm', e.target.value)}>
-                <option value="E">E</option>
-                <option value="W">W</option>
-              </select>
-            </div>
-          </div>
-          <hr />
-          <div className="dms-wrapper">
-            <span>{latitude}</span>
-            <div className="input-wrapper">
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{degree}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{apostrophe}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{quotes}</span>
-              <select onChange={e => console.log('hmmm', e.target.value)}>
-                <option value="N">N</option>
-                <option value="S">S</option>
-              </select>
-            </div>
-          </div>
-          <div className="dms-wrapper">
-            <span>{longitude}</span>
-            <div className="input-wrapper">
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{degree}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{apostrophe}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{quotes}</span>
-              <select onChange={e => console.log('hmmm', e.target.value)}>
-                <option value="E">E</option>
-                <option value="W">W</option>
-              </select>
-            </div>
-          </div>
-          <hr />
-          <div className="dms-wrapper">
-            <span>{latitude}</span>
-            <div className="input-wrapper">
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{degree}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{apostrophe}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{quotes}</span>
-              <select onChange={e => console.log('hmmm', e.target.value)}>
-                <option value="N">N</option>
-                <option value="S">S</option>
-              </select>
-            </div>
-          </div>
-          <div className="dms-wrapper">
-            <span>{longitude}</span>
-            <div className="input-wrapper">
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{degree}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{apostrophe}</span>
-              <input type="number" name="latitude coordinates" />
-              <span className="degree">{quotes}</span>
-              <select onChange={e => console.log('hmmm', e.target.value)}>
-                <option value="E">E</option>
-                <option value="W">W</option>
-              </select>
-            </div>
-          </div>
-          <hr />
-          <div className="buttons-wrapper">
-            <button>Add more</button>
-            <button className="orange-button">Make shape</button>
-          </div>
-        </>
-      );
-    }
+      const rows = [1, 2, 3, 4, 5, 6];
 
-    if (decimalOptions[selectedFormat].includes('DD')) {
-      return (
-        <>
-          <div className="dds-wrapper">
-            <div className="dds-input">
-              <p>{latitude}</p>
-              <div className="degree-input">
-                <input type="number" name="latitude coordinates" />
-                <span>{degree}</span>
+      return rows.map(rowNum => {
+        if (rowNum % 2 === 1) {
+          return (
+            <>
+              <div className="dms-wrapper">
+                <span>{latitude}</span>
+                <div className="input-wrapper">
+                  <input type="number" name="latitude coordinates" />
+                  <span className="degree">{degree}</span>
+                  <input type="number" name="latitude coordinates" />
+                  <span className="degree">{apostrophe}</span>
+                  <input type="number" name="latitude coordinates" />
+                  <span className="degree">{quotes}</span>
+                  <select onChange={e => console.log('hmmm', e.target.value)}>
+                    <option value="N">N</option>
+                    <option value="S">S</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="dds-input">
-              <p>{longitude}</p>
-              <div className="degree-input">
-                <input type="number" name="longitude coordinates" />
-                <span>{degree}</span>
+            </>
+          );
+        } else {
+          return (
+            <>
+              <div className="dms-wrapper">
+                <span>{longitude}</span>
+                <div className="input-wrapper">
+                  <input type="number" name="latitude coordinates" />
+                  <span className="degree">{degree}</span>
+                  <input type="number" name="latitude coordinates" />
+                  <span className="degree">{apostrophe}</span>
+                  <input type="number" name="latitude coordinates" />
+                  <span className="degree">{quotes}</span>
+                  <select onChange={e => console.log('hmmm', e.target.value)}>
+                    <option value="E">E</option>
+                    <option value="W">W</option>
+                  </select>
+                </div>
               </div>
-            </div>
-          </div>
-          <hr />
-          {/* <div className="dds-wrapper">
-            <div className="dds-input">
-              <p>{latitude}</p>
-              <div className="degree-input">
-                <input type="number" name="latitude coordinates" />
-                <span>{degree}</span>
-              </div>
-            </div>
-            <div className="dds-input">
-              <p>{longitude}</p>
-              <div className="degree-input">
-                <input type="number" name="longitude coordinates" />
-                <span>{degree}</span>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="dds-wrapper">
-            <div className="dds-input">
-              <p>{latitude}</p>
-              <div className="degree-input">
-                <input type="number" name="latitude coordinates" />
-                <span>{degree}</span>
-              </div>
-            </div>
-            <div className="dds-input">
-              <p>{longitude}</p>
-              <div className="degree-input">
-                <input type="number" name="longitude coordinates" />
-                <span>{degree}</span>
-              </div>
-            </div>
-          </div> */}
-          <hr />
-        </>
-      );
+              <hr />
+            </>
+          );
+        }
+      });
     }
   };
+
+  const returnSelectedContent = (): ReactElement => {
+    return (
+      <>
+        {latitudeLongitudeRow()}
+        <div className="buttons-wrapper">
+          <button>Add more</button>
+          <button className="orange-button">Make shape</button>
+        </div>
+      </>
+    );
+  };
+
+  // if (decimalOptions[selectedFormat].includes('DD')) {
+  //   return (
+  //     <>
+  //       <div className="dds-wrapper">
+  //         <div className="dds-input">
+  //           <p>{latitude}</p>
+  //           <div className="degree-input">
+  //             <input type="number" name="latitude coordinates" />
+  //             <span>{degree}</span>
+  //           </div>
+  //         </div>
+  //         <div className="dds-input">
+  //           <p>{longitude}</p>
+  //           <div className="degree-input">
+  //             <input type="number" name="longitude coordinates" />
+  //             <span>{degree}</span>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <hr />
+  //       {/* <div className="dds-wrapper">
+  //         <div className="dds-input">
+  //           <p>{latitude}</p>
+  //           <div className="degree-input">
+  //             <input type="number" name="latitude coordinates" />
+  //             <span>{degree}</span>
+  //           </div>
+  //         </div>
+  //         <div className="dds-input">
+  //           <p>{longitude}</p>
+  //           <div className="degree-input">
+  //             <input type="number" name="longitude coordinates" />
+  //             <span>{degree}</span>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <hr />
+  //       <div className="dds-wrapper">
+  //         <div className="dds-input">
+  //           <p>{latitude}</p>
+  //           <div className="degree-input">
+  //             <input type="number" name="latitude coordinates" />
+  //             <span>{degree}</span>
+  //           </div>
+  //         </div>
+  //         <div className="dds-input">
+  //           <p>{longitude}</p>
+  //           <div className="degree-input">
+  //             <input type="number" name="longitude coordinates" />
+  //             <span>{degree}</span>
+  //           </div>
+  //         </div>
+  //       </div> */}
+  //       <hr />
+  //     </>
+  //   );
+  // }
+  // };
 
   return (
     <div className="coordinates-form-container">
