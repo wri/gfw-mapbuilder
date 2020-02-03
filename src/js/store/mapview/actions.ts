fd11dd2a626f95130225b4e922579204fc9ba7e9
Vaidotas Piekus <vaidotasp@gmail.com>
2020-02-03 09:51:55 -0500

    redux update for allAvailableLayers usage

--- src/js/store/mapview/actions.ts
+++ src/js/store/mapview/actions.ts
@@ -1,11 +1,8 @@
 import {
   MAP_ERROR,
   MAP_READY,
-  CLEAR_ALL_LAYERS,
-  UPDATE_VISIBLE_LAYERS,
   ALL_AVAILABLE_LAYERS,
-  MapviewState,
-  ALL_AVAILABLE_LAYERS2
+  MapviewState
 } from './types';
 
 export function isMapReady(payload: MapviewState['isMapReady']) {
@@ -22,33 +19,11 @@ export function mapError(payload: MapviewState['loadError']) {
   };
 }
 
-export function clearAllLayers() {
-  return {
-    type: CLEAR_ALL_LAYERS as typeof CLEAR_ALL_LAYERS
-  };
-}
-
-export function updateVisibleLayers(payload: MapviewState['visibleLayers']) {
-  return {
-    type: UPDATE_VISIBLE_LAYERS as typeof UPDATE_VISIBLE_LAYERS,
-    payload
-  };
-}
-
 export function allAvailableLayers(
   payload: MapviewState['allAvailableLayers']
 ) {
   return {
     type: ALL_AVAILABLE_LAYERS as typeof ALL_AVAILABLE_LAYERS,
-    payload
-  };
-}
-
-export function allAvailableLayers2(
-  payload: MapviewState['allAvailableLayers2']
-) {
-  return {
-    type: ALL_AVAILABLE_LAYERS2 as typeof ALL_AVAILABLE_LAYERS2,
     payload
   };
 }
--- src/js/store/mapview/types.ts
+++ src/js/store/mapview/types.ts
@@ -2,9 +2,7 @@
 export interface MapviewState {
   isMapReady: boolean;
   loadError: boolean;
-  visibleLayers: string[];
-  allAvailableLayers: string[];
-  allAvailableLayers2: LayerProps[];
+  allAvailableLayers: LayerProps[];
 }
 
 export interface LayerProps {
@@ -19,10 +17,7 @@ export interface LayerProps {
 //Action types
 export const MAP_READY = 'MAP_READY';
 export const MAP_ERROR = 'MAP_ERROR';
-export const CLEAR_ALL_LAYERS = 'CLEAR_ALL_LAYERS';
-export const UPDATE_VISIBLE_LAYERS = 'UPDATE_VISIBLE_LAYERS';
 export const ALL_AVAILABLE_LAYERS = 'ALL_AVAILABLE_LAYERS';
-export const ALL_AVAILABLE_LAYERS2 = 'ALL_AVAILABLE_LAYERS2';
 
 interface MapIsReadyAction {
   type: typeof MAP_READY;
@@ -34,29 +29,12 @@ interface MapErrorAction {
   payload: boolean;
 }
 
-interface ClearAllLayersAction {
-  type: typeof CLEAR_ALL_LAYERS;
-}
-
-interface UpdateVisibleLayersAction {
-  type: typeof UPDATE_VISIBLE_LAYERS;
-  payload: MapviewState['visibleLayers'];
-}
-
 interface AllAvailableLayersAction {
   type: typeof ALL_AVAILABLE_LAYERS;
   payload: MapviewState['allAvailableLayers'];
 }
 
-interface AllAvailableLayersAction2 {
-  type: typeof ALL_AVAILABLE_LAYERS2;
-  payload: MapviewState['allAvailableLayers2'];
-}
-
 export type MapviewStateTypes =
   | MapIsReadyAction
   | MapErrorAction
-  | ClearAllLayersAction
-  | UpdateVisibleLayersAction
-  | AllAvailableLayersAction
-  | AllAvailableLayersAction2;
+  | AllAvailableLayersAction;
