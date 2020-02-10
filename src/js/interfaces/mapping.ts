471d9173841f072caf921f5c176dbd34890d7a6f
Lucas Cotner <lc07@uw.edu>
2020-02-10 09:12:09 -0500

    Hoisted mapping interfaces

---
+++ src/js/interfaces/mapping.ts
@@ -0,0 +1,9 @@
+export interface LayerFactoryObject {
+    id: string;
+    title: string;
+    opacity: number;
+    visible: boolean;
+    definitionExpression: string | undefined;
+    url: string;
+    type: string;
+}
