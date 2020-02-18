2f9b78d8bd7aabd014f06e1a819f78ee62d203d0
Eden <eden.s.mesfun@gmail.com>
2020-02-18 10:59:30 -0500

    added range for lat/long, and form validation logic.

--- src/js/components/mapWidgets/widgetContent/searchContent.tsx
+++ src/js/components/mapWidgets/widgetContent/searchContent.tsx
@@ -15,6 +15,7 @@ import { searchContent } from '../../../../../configs/modal.config';
 
 const SearchContent: FunctionComponent = () => {
   const searchRef = useRef(null);
+  const [formError, setFormError] = useState(false);
   const [latitudeInput, setLatitude] = useState('');
   const [longitudeInput, setLongitude] = useState('');
   const selectedLanguage = useSelector(
@@ -29,18 +30,30 @@ const SearchContent: FunctionComponent = () => {
   }, [searchRef]);
 
   const setSearch = (): void => {
-    mapController.setSearchWidget(latitudeInput, longitudeInput);
+    if (Number(latitudeInput) < 0 || Number(latitudeInput) > 90) {
+      setFormError(true);
+    } else if (Number(latitudeInput) < 0 || Number(latitudeInput) > 90) {
+      setFormError(true);
+    } else {
+      setFormError(false);
+      mapController.setSearchWidget(latitudeInput, longitudeInput);
+    }
   };
 
   return (
     <div className="modal-content-container">
       <div className="directions">
         <div className="form-wrapper">
+          {formError && (
+            <p>Latitudes range from 0 to 90. Longitudes range from 0 to 180.</p>
+          )}
           <label htmlFor={latitude}>{latitude}:</label>
           <input
             value={latitudeInput}
             id={latitude}
             type="number"
+            min={0}
+            max={90}
             className="input-coordinates"
             onChange={(e: ChangeEvent<HTMLInputElement>): void =>
               setLatitude(e.target.value)
@@ -52,6 +65,8 @@ const SearchContent: FunctionComponent = () => {
             value={longitudeInput}
             id={longitude}
             type="number"
+            min={0}
+            max={180}
             className="input-coordinates"
             onChange={(e: ChangeEvent<HTMLInputElement>): void =>
               setLongitude(e.target.value)
