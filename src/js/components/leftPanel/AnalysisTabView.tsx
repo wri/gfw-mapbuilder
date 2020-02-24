dc7e2b8ef33084c273dc12ff33a6eee7bd71e4b8
Lucas Cotner <lc07@uw.edu>
2020-02-24 14:10:38 -0500

    Double-stache!

--- src/js/components/leftPanel/AnalysisTabView.tsx
+++ src/js/components/leftPanel/AnalysisTabView.tsx
@@ -6,20 +6,41 @@ interface Props {
   key: string;
   label: string;
 }
+
+interface TabProps {
+  key: string;
+  label: string;
+}
+
+const DefaultTabView = (): JSX.Element => (
+  <div className="data-tab-default-container">
+    <p>Select a shape on the map</p>
+  </div>
+);
+
+const AnalysisComp = (): JSX.Element => (
+  <div className="data-tab-default-container">
+    <p>Select a shape on the map</p>
+  </div>
+);
+
 const AnalysisTabView = (props: Props) => {
   const { activeTab, tabViewVisible } = useSelector(
     (store: RootState) => store.appState.leftPanel
   );
 
-  const { activeFeatures } = useSelector(
+  const { activeFeatures, activeFeatureIndex } = useSelector(
     (store: RootState) => store.mapviewState
   );
 
   console.log('activeFeatures', activeFeatures);
+  console.log('activeFeatureIndex', activeFeatureIndex);
+  //activeFeatures[activeLayerIndex].features
+  //const activeLayer = activeFeatures[activeFeatureIndex[0]].layerID;
 
   const tabViewIsVisible = tabViewVisible && activeTab === props.label;
   console.log('tabViewIsVisible', tabViewIsVisible);
-  return <>{tabViewIsVisible && <div>Analysis Tab View</div>}</>;
+  return tabViewIsVisible ? DefaultTabView : <RegisterSpin />;
 };
 
 export default AnalysisTabView;
