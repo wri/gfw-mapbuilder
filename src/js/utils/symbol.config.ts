900a418bbfd8ee345acbfcab92425d970ad5535b
Eden <eden.s.mesfun@gmail.com>
2020-02-27 14:49:14 -0500

    removed conditional logic since it was superfluous

--- src/js/utils/symbol.config.ts
+++ src/js/utils/symbol.config.ts
@@ -1,38 +1,25 @@
 import SimpleFillSymbol from 'esri/symbols/SimpleFillSymbol';
 
-let customSymbol: undefined | SimpleFillSymbol = undefined;
-let imagerySymbol: undefined | SimpleFillSymbol = undefined;
-
-export const getCustomSymbol = (): any => {
-  if (customSymbol) {
-    return customSymbol;
-  } else {
-    customSymbol = new SimpleFillSymbol({
-      style: 'solid',
-      color: [210, 210, 210, 0.0],
-      outline: {
-        // autocasts as new SimpleLineSymbol()
-        color: [3, 188, 255],
-        width: 3
-      }
-    });
-    return customSymbol;
-  }
+export const getCustomSymbol = (): SimpleFillSymbol => {
+  return new SimpleFillSymbol({
+    style: 'solid',
+    color: [210, 210, 210, 0.0],
+    outline: {
+      // autocasts as new SimpleLineSymbol()
+      color: [3, 188, 255],
+      width: 3
+    }
+  });
 };
 
-export const getImagerySymbol = (): any => {
-  if (imagerySymbol) {
-    return imagerySymbol;
-  } else {
-    imagerySymbol = new SimpleFillSymbol({
-      style: 'solid',
-      color: [210, 210, 210, 0.0],
-      outline: {
-        // autocasts as new SimpleLineSymbol()
-        color: [210, 210, 210, 0],
-        width: 1
-      }
-    });
-    return imagerySymbol;
-  }
+export const getImagerySymbol = (): SimpleFillSymbol => {
+  return new SimpleFillSymbol({
+    style: 'solid',
+    color: [210, 210, 210, 0.0],
+    outline: {
+      // autocasts as new SimpleLineSymbol()
+      color: [210, 210, 210, 0],
+      width: 1
+    }
+  });
 };
