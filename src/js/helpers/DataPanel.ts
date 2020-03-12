223a12884c82c6d8a6f6e00c15f7a083eb937772
Vaidotas Piekus <vaidotasp@gmail.com>
2020-03-12 15:06:59 -0400

    data panel rework for proper attribute fetching

--- src/js/helpers/DataPanel.ts
+++ src/js/helpers/DataPanel.ts
@@ -1,5 +1,4 @@
 import MapView from 'esri/views/MapView';
-import Point from 'esri/geometry/Point';
 import Map from 'esri/Map';
 import store from 'js/store';
 import QueryTask from 'esri/tasks/QueryTask';
@@ -18,42 +17,38 @@ function esriQuery(url: string, queryParams: any): Promise<__esri.FeatureSet> {
   return result;
 }
 
-//Helper to fetch name field for the objectids due to inconsistent naming and application requiring object id for attachment fetching
-async function getLayerFields(
+//Helper fn to get all available layer fields for the data tab. If layer has fields itself we are using those, but if that is not the case, attempt to fetch it by hitting layer url endpoint with pjson prefix
+async function getAllLayerFields(
   layer: __esri.FeatureLayer
-): Promise<__esri.Field | undefined> {
-  let layerObjectField = {} as __esri.Field | undefined;
+): Promise<__esri.Field[] | undefined> {
+  let layerFields = [] as __esri.Field[] | undefined;
   if (layer.fields) {
-    layerObjectField = layer.fields.find(
-      field =>
-        field.type.toLowerCase() === 'esrifieldtypeoid' ||
-        field.type.toLowerCase() === 'oid'
-    );
+    layerFields = layer.fields;
   } else {
-    layerObjectField = await fetch(`${layer.url}?f=pjson`, {
+    layerFields = await fetch(`${layer.url}?f=pjson`, {
       body: null,
       method: 'GET'
     })
       .then(response => response.json())
       .then(data => {
         if (data?.fields) {
-          const objectidName = data.fields.find(
-            (field: __esri.Field) =>
-              field.type.toLowerCase() === 'esrifieldtypeoid' ||
-              field.type.toLowerCase() === 'oid'
-          );
-          return objectidName;
+          return data.fields;
         }
       });
   }
-  return layerObjectField;
+  return layerFields;
 }
 
+//Helper fn to grab WHICH attributes we want to show in the data panel. Those can be coming from either popupTemplate in case of webmaps or it can also come from layer metadata which is fetched at the layer loading point. This is the case with most non webmap layers.
 interface FieldInfo {
   fieldName: string;
   label: string;
 }
-function getAttributesToFetch(layer: __esri.FeatureLayer): FieldInfo[] | null {
+function getAttributesToFetch(
+  layer: __esri.FeatureLayer | __esri.Sublayer,
+  isSubLayer?: boolean,
+  availableFields?: __esri.Field[] | undefined
+): FieldInfo[] | null {
   //Check for popupTemplate > this handles webmap layers
   let enabledFieldInfos: FieldInfo[] | null = null;
   if (layer.popupTemplate) {
@@ -67,10 +62,50 @@ function getAttributesToFetch(layer: __esri.FeatureLayer): FieldInfo[] | null {
       });
   } else {
     //No popup template found, check the metadata
-    debugger;
-    enabledFieldInfos = [{ fieldName: '*', label: '*' }];
-    console.log('no popupTemplate found, lets dig into metadata');
-    console.log(layer);
+    const { allAvailableLayers } = store.getState().mapviewState;
+    const { selectedLanguage } = store.getState().appState;
+    //@ts-ignore -- weird issue when sublayer suppose to have layer prop but TS does not agree
+    const layerID = isSubLayer ? layer.layer.id : layer.id;
+    const layerInfo = allAvailableLayers.find(l => l.id === layerID);
+
+    //TODO: There is a situation where metada has a field, but layer itself does not, we need to filter those out
+    const availableFieldsNames = availableFields?.map(f =>
+      f.name.toLowerCase().trim()
+    );
+
+    //TODO: 3x supported modifiers on the fieldName eg ACQ_DATE:DateString(hideTime:true), 4x this is no longer supported and modifiers should be added to FieldInfoFormat class. For now we are going to be sanitizing fieldNames that come from metadata (gfw api) so queries do not break. But this would need to be changed on their api end
+    const fieldsFromMetadataCleaned = layerInfo?.popup?.content[
+      selectedLanguage
+    ].map((f: any) => {
+      const newFieldName = f.fieldExpression.includes(':')
+        ? f.fieldExpression.substring(0, f.fieldExpression.indexOf(':'))
+        : f.fieldExpression;
+      return { fieldName: newFieldName, label: f.label };
+    });
+
+    //how do we handle no metadata on popup?
+    const fieldNamesFromMetadata = fieldsFromMetadataCleaned?.map((f: any) => {
+      return f.fieldName.toLowerCase().trim();
+    });
+    // Deal with matching names from metadata and from layer itself
+    if (availableFieldsNames && fieldNamesFromMetadata) {
+      const attributeFieldsToInclude = fieldNamesFromMetadata
+        .filter((f: any) => availableFieldsNames.includes(f))
+        .map((f: any) => {
+          const matchingLabel = availableFields?.find(
+            (allowedField: any) => allowedField.name.toLowerCase() === f
+          );
+          return {
+            fieldName: f,
+            label: matchingLabel?.alias
+          };
+        });
+
+      enabledFieldInfos =
+        attributeFieldsToInclude && attributeFieldsToInclude.length > 0
+          ? attributeFieldsToInclude
+          : [{ fieldName: '*', label: '*' }];
+    }
   }
   return enabledFieldInfos;
 }
@@ -78,7 +113,8 @@ function getAttributesToFetch(layer: __esri.FeatureLayer): FieldInfo[] | null {
 async function fetchQueryTask(
   layer: __esri.FeatureLayer,
   mapview: MapView,
-  event: __esri.MapViewClickEvent
+  event: __esri.MapViewClickEvent,
+  isSubLayer?: boolean
 ): Promise<FeatureResult[]> {
   let featureResult = [] as FeatureResult[];
   const queryParams: any = {
@@ -91,13 +127,26 @@ async function fetchQueryTask(
   };
   const url = layer.url;
   try {
-    const layerFields = await getLayerFields(layer);
-    if (layerFields?.name) {
-      queryParams.outFields.push(layerFields.name);
+    const allLayerFields = await getAllLayerFields(layer);
+    let objectid: string | null = null;
+    if (allLayerFields) {
+      const layerObjectField = allLayerFields.find(
+        field =>
+          field.type.toLowerCase() === 'esrifieldtypeoid' ||
+          field.type.toLowerCase() === 'oid'
+      );
+      if (layerObjectField?.name) {
+        objectid = layerObjectField?.name;
+        queryParams.outFields = [layerObjectField.name];
+      }
     }
-    const attributesToFetch = getAttributesToFetch(layer);
+    const attributesToFetch = getAttributesToFetch(
+      layer,
+      isSubLayer,
+      allLayerFields
+    );
     const newOutFields = attributesToFetch?.map(f => f.fieldName);
-    queryParams.outFields.push(newOutFields);
+    queryParams.outFields = queryParams.outFields.concat(newOutFields);
     const sublayerResult = await esriQuery(url, queryParams);
 
     if (sublayerResult.features.length > 0) {
@@ -105,7 +154,7 @@ async function fetchQueryTask(
         return {
           attributes: f.attributes,
           geometry: f.geometry,
-          objectid: layerFields?.name ? f.attributes[layerFields.name] : null
+          objectid: objectid ? f.attributes[objectid] : null
         };
       });
     }
@@ -130,9 +179,18 @@ async function fetchQueryFeatures(
     returnGeometry: true
   };
   const attributesToFetch = getAttributesToFetch(layer);
-  const layerFields = await getLayerFields(layer);
-  if (layerFields?.name) {
-    queryParams.outFields.push(layerFields.name);
+  const allLayerFields = await getAllLayerFields(layer);
+  let objectid: string | null = null;
+  if (allLayerFields) {
+    const layerObjectField = allLayerFields.find(
+      field =>
+        field.type.toLowerCase() === 'esrifieldtypeoid' ||
+        field.type.toLowerCase() === 'oid'
+    );
+    if (layerObjectField?.name) {
+      objectid = layerObjectField?.name;
+      queryParams.outFields = [layerObjectField.name];
+    }
   }
   const newOutFields = attributesToFetch?.map(f => f.fieldName);
   queryParams.outFields.push(newOutFields);
@@ -144,7 +202,7 @@ async function fetchQueryFeatures(
         return {
           attributes: f.attributes,
           geometry: f.geometry,
-          objectid: layerFields?.name ? f.attributes[layerFields.name] : null
+          objectid: objectid ? f.attributes[objectid] : null
         };
       });
     }
@@ -154,7 +212,7 @@ async function fetchQueryFeatures(
   return featureResult;
 }
 
-//Client Side Feature Fetching
+//Feature Fetching Logic starts
 export async function queryLayersForFeatures(
   mapview: MapView,
   map: Map | undefined,
@@ -180,7 +238,7 @@ export async function queryLayersForFeatures(
         for (const sublayer of layer.sublayers.items) {
           //sublayers do not have a type, so it always defaults to QueryTask
           //use generic QueryTask approach
-          const features = await fetchQueryTask(sublayer, mapview, event);
+          const features = await fetchQueryTask(sublayer, mapview, event, true);
           if (features.length > 0) {
             layerFeatureResults.push({
               layerID: layer.id,
@@ -212,7 +270,7 @@ export async function queryLayersForFeatures(
           }
         } else {
           //use generic QueryTask approach
-          const features = await fetchQueryTask(layer, mapview, event);
+          const features = await fetchQueryTask(layer, mapview, event, false);
           if (features.length > 0) {
             layerFeatureResults.push({
               layerID: layer.id,
