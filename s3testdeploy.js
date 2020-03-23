c785f5d419627f20e0a1f788c2c385d881732067
Vaidotas Piekus <vaidotasp@gmail.com>
2020-03-23 14:02:01 -0400

    Fix eslint rule duplication and deploy script default editor

--- s3testdeploy.js
+++ s3testdeploy.js
@@ -9,7 +9,7 @@
     * Make sure you got AWS CLI configured because it uses it to push to S3 
 */
 
-const cp = require('child_process');
+const cp = require("child_process");
 
 const execSync = (...args) =>
   cp
@@ -19,25 +19,23 @@ const execSync = (...args) =>
 
 // Set these globals up, friend.
 const config = {
-  bucket: 'alpha.blueraster.io/',
-  directory: 'wcs/test/',
-  projectDir: 'gfw-mapbuilder', //the name of the project folder on your machine
-  buildCommand: 'npm run build', //switch to npm or whatever you use to run build command
+  bucket: "alpha.blueraster.io/",
+  directory: "wcs/test/",
+  projectDir: "gfw-mapbuilder", //the name of the project folder on your machine
+  buildCommand: "npm run build", //switch to npm or whatever you use to run build command
   get awsS3Command() {
-    return `aws s3 sync --acl public-read --include 'webpackBuild/' webpackBuild s3://${
-      this.bucket
-    }${this.directory}`; // you can edit this if you have different s3 sync setup
+    return `aws s3 sync --acl public-read --include 'webpackBuild/' webpackBuild s3://${this.bucket}${this.directory}`; // you can edit this if you have different s3 sync setup
   }
 };
 
 //helper fn to copy output txt to clipboard
 function pbcopy(data) {
   return new Promise(function(resolve, reject) {
-    const proc = require('child_process').spawn('pbcopy');
-    proc.on('error', function(err) {
+    const proc = require("child_process").spawn("pbcopy");
+    proc.on("error", function(err) {
       reject(err);
     });
-    proc.on('close', function(err) {
+    proc.on("close", function(err) {
       resolve();
     });
     proc.stdin.write(data);
@@ -53,43 +51,39 @@ function main() {
   for (const prop in config) {
     if (config[prop] === null) {
       throw new Error(
-        `You forgot to set up the global variable, friend. ${prop} is: ${
-          config[prop]
-        }`
+        `You forgot to set up the global variable, friend. ${prop} is: ${config[prop]}`
       );
     }
   }
 
   // let's make sure we are in correct dir
-  const currentDir = execSync('pwd');
-  const regex = new RegExp(config.projectDir, 'gm');
+  const currentDir = execSync("pwd");
+  const regex = new RegExp(config.projectDir, "gm");
   if (!currentDir.match(regex)) {
     console.log(`You are in ${currentDir}`);
     throw new Error(
-      'Not in the intended directory. This script only works in the directory that you indicated as the _base_. Navigate to the intended directory and run it again'
+      "Not in the intended directory. This script only works in the directory that you indicated as the _base_. Navigate to the intended directory and run it again"
     );
   }
 
   //check if directory is not empty
   if (
-    config.directory === '' ||
+    config.directory === "" ||
     config.directory === null ||
-    config.directory === '*'
+    config.directory === "*"
   ) {
     throw new Error(
-      'Wow there, looks like directory is empty, dont just nuke the whole bucket plz.'
+      "Wow there, looks like directory is empty, dont just nuke the whole bucket plz."
     );
   }
 
-  const deployCommand = `${config.buildCommand} && ${
-    config.awsS3Command
-  }${branchName}`;
-  cp.execSync(deployCommand, { stdio: 'inherit' });
+  const deployCommand = `${config.buildCommand} && ${config.awsS3Command}${branchName}`;
+  cp.execSync(deployCommand, { stdio: "inherit" });
   const testURL = `https://${config.bucket}${config.directory}${branchName}/`;
   pbcopy(testURL).then(function() {
-    console.log('URL Copied to clipboard');
+    console.log("URL Copied to clipboard");
   });
-  execSync(`open -a "Google Chrome" ${testURL}`);
+  execSync(`open ${testURL}`);
   return;
 }
 
