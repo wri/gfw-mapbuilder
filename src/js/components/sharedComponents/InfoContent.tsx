29cd838d483ae04d78448bf302962f2b7165113c
Eden <eden.s.mesfun@gmail.com>
2020-03-31 13:33:07 -0400

    leveraged xml-js for XML fetching. Fetch description/copyright for WEBMAP_GROUP

--- src/js/components/sharedComponents/InfoContent.tsx
+++ src/js/components/sharedComponents/InfoContent.tsx
@@ -1,9 +1,17 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+// import convert from 'xml-js';
 import { useSelector } from 'react-redux';
+
 import { RootState } from 'js/store';
+
 import { downloadData } from '../../../../configs/modal.config';
 
-const InfoContent: FunctionComponent<{}> = () => {
+const InfoContent: FunctionComponent<{}> = (): any => {
+  const [customContent, setCustomContent] = useState({
+    description: '',
+    copyrightText: ''
+  });
+
   const { infoModalLayerID: layerID, selectedLanguage } = useSelector(
     (store: RootState) => store.appState
   );
@@ -15,122 +23,175 @@ const InfoContent: FunctionComponent<{}> = () => {
     (layer: any) => layer.id === layerID
   )[0];
 
-  const RenderLayerContent = (): JSX.Element => {
-    if (layer.metadata) {
-      const { metadata } = layer.metadata as any;
-      const {
-        resolution,
-        tags,
-        geographic_coverage,
-        source,
-        frequency_of_updates,
-        date_of_content,
-        cautions,
-        license,
-        overview,
-        citation,
-        title,
-        subtitle,
-        download_data
-      } = metadata;
+  const getWebmapGroupContent = async (): Promise<any> => {
+    // TODO [ ] IF metadata exists from ArcGIS, use it!
+    //    const metadataURL = `${layer.url}/info/metadata`;
+    //    const xmlResults = await fetch(metadataURL).then(res => res.text());
+    //    const results = convert.xml2js(xmlResults);
+    //
+    // TODO [ ] ELSE, use the description and summary instead
 
-      return (
-        <>
-          <div className="header">
-            <h2>{title}</h2>
-            <h3>{subtitle}</h3>
-          </div>
+    return await fetch(`${layer.url}/?f=pjson`)
+      .then(res => res.json())
+      .then(results => {
+        return {
+          description: results.description,
+          copyrightText: results.copyrightText
+        };
+      });
+  };
+
+  useEffect(() => {
+    const grabContent = async (): Promise<void> => {
+      const contentResults = await getWebmapGroupContent();
+      setCustomContent(contentResults);
+    };
+
+    if (layer.type === 'webmap') {
+      grabContent();
+    }
+  }, []);
+
+  const returnWebmapGroupContent = (): JSX.Element => {
+    const { description, copyrightText } = customContent;
+
+    return (
+      <>
+        <div className="header">
+          <h2>{layer.title}</h2>
           <table>
             <tbody>
               <tr>
-                <td className="label">Function</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: metadata.function }}
-                />
-              </tr>
-              <tr>
-                <td className="label">Resolution</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: resolution }}
-                />
-              </tr>
-              <tr>
-                <td className="label">Tags</td>
-                <td className="label-info">{tags}</td>
-              </tr>
-              <tr>
-                <td className="label">Geographic Coverage</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: geographic_coverage }}
-                />
-              </tr>
-              <tr>
-                <td className="label">Source</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: source }}
-                />
-              </tr>
-              <tr>
-                <td className="label">Frequency</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: frequency_of_updates }}
-                />
-              </tr>
-              <tr>
-                <td className="label">Date of Content</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: date_of_content }}
-                />
-              </tr>
-              <tr>
-                <td className="label">Cautions</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: cautions }}
-                />
+                <td className="label">Description</td>
+                <td className="label-info">{description}</td>
               </tr>
               <tr>
                 <td className="label">License</td>
-                <td
-                  className="label-info"
-                  dangerouslySetInnerHTML={{ __html: license }}
-                />
+                <td className="label-info">{copyrightText}</td>
               </tr>
             </tbody>
           </table>
-          <div className="overview-container">
-            <h3>Overview</h3>
-            <div dangerouslySetInnerHTML={{ __html: overview }} />
+        </div>
+      </>
+    );
+  };
+
+  const returnOtherGroupContent = (): JSX.Element => {
+    const { metadata } = layer.metadata as any;
+    const {
+      resolution,
+      tags,
+      geographic_coverage,
+      source,
+      frequency_of_updates,
+      date_of_content,
+      cautions,
+      license,
+      overview,
+      citation,
+      title,
+      subtitle,
+      download_data
+    } = metadata;
+
+    return (
+      <>
+        <div className="header">
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td className="label">Function</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: metadata.function }}
+              />
+            </tr>
+            <tr>
+              <td className="label">Resolution</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: resolution }}
+              />
+            </tr>
+            <tr>
+              <td className="label">Tags</td>
+              <td className="label-info">{tags}</td>
+            </tr>
+            <tr>
+              <td className="label">Geographic Coverage</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: geographic_coverage }}
+              />
+            </tr>
+            <tr>
+              <td className="label">Source</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: source }}
+              />
+            </tr>
+            <tr>
+              <td className="label">Frequency</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: frequency_of_updates }}
+              />
+            </tr>
+            <tr>
+              <td className="label">Date of Content</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: date_of_content }}
+              />
+            </tr>
+            <tr>
+              <td className="label">Cautions</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: cautions }}
+              />
+            </tr>
+            <tr>
+              <td className="label">License</td>
+              <td
+                className="label-info"
+                dangerouslySetInnerHTML={{ __html: license }}
+              />
+            </tr>
+          </tbody>
+        </table>
+        <div className="overview-container">
+          <h3>Overview</h3>
+          <div dangerouslySetInnerHTML={{ __html: overview }} />
+        </div>
+        <div className="citation-container">
+          <h4>Citation</h4>
+          <div dangerouslySetInnerHTML={{ __html: citation }} />
+        </div>
+        {download_data && (
+          <div className="button-container">
+            <a href={download_data} target="_blank" rel="noopener noreferrer">
+              <button className="orange-button">
+                {downloadData[selectedLanguage]
+                  ? downloadData[selectedLanguage]
+                  : 'Download Data'}
+              </button>
+            </a>
           </div>
-          <div className="citation-container">
-            <h4>Citation</h4>
-            <div dangerouslySetInnerHTML={{ __html: citation }} />
-          </div>
-          {download_data && (
-            <div className="button-container">
-              <a href={download_data} target="_blank" rel="noopener noreferrer">
-                <button className="orange-button">
-                  {downloadData[selectedLanguage]
-                    ? downloadData[selectedLanguage]
-                    : 'Download Data'}
-                </button>
-              </a>
-            </div>
-          )}
-        </>
-      );
+        )}
+      </>
+    );
+  };
+
+  const RenderLayerContent = (): JSX.Element => {
+    if (layer.type === 'webmap') {
+      return returnWebmapGroupContent();
     } else {
-      console.log(
-        "This layer doesn't have metadata to support this feature!",
-        layer
-      );
-      return <></>;
+      return returnOtherGroupContent();
     }
   };
 
