1f4ae1e1e79d588359c82cdb673fc7a1ce31e39c
Vaidotas Piekus <vaidotasp@gmail.com>
2020-04-17 15:18:46 -0400

    Input styling changes

--- src/js/components/ReportChartsComponent.tsx
+++ src/js/components/ReportChartsComponent.tsx
@@ -3,6 +3,12 @@ import { RootState } from 'js/store';
 import { createSelector } from 'reselect';
 import { useSelector } from 'react-redux';
 import { AnalysisModule } from 'js/store/appSettings/types';
+import {
+  MemoRangeSlider,
+  MemoDatePicker
+} from 'js/components/leftPanel/analysisPanel/InputComponents';
+import CanopyDensityPicker from 'js/components/sharedComponents/CanopyDensityPicker';
+import { UIParams } from 'js/components/leftPanel/analysisPanel/BaseAnalysis';
 
 import { ReactComponent as GearIcon } from '../../../images/gearIcon.svg';
 import { ReactComponent as DownloadIcon } from '../../../images/downloadIcon.svg';
@@ -17,13 +23,51 @@ interface ChartModuleProps {
   lang: string;
 }
 const ChartModule = (props: ChartModuleProps): JSX.Element => {
-  console.log(props);
-  const language = props.lang;
   const { label } = props.moduleInfo;
+  const language = props.lang;
+  const translatedLabel = label[language]
+    ? label[language]
+    : 'Missing Translation Analysis Label';
+  console.log(props);
+  const currentAnalysis = props.moduleInfo;
+  const [submoduleIsHidden, setSubmoduleIsHidden] = React.useState(false);
+
+  const renderInputComponent = (
+    props: UIParams
+  ): JSX.Element | null | undefined => {
+    const {
+      multi,
+      minDate,
+      maxDate,
+      defaultStartDate,
+      defaultEndDate,
+      bounds
+    } = props;
+    switch (props.inputType) {
+      case 'rangeSlider':
+        if (bounds) return <MemoRangeSlider yearRange={bounds} />;
+        break;
+      case 'tcd':
+        return <CanopyDensityPicker label={false} />;
+      case 'datepicker':
+        return (
+          <MemoDatePicker
+            multi={multi}
+            minDate={minDate}
+            maxDate={maxDate}
+            defaultStartDate={defaultStartDate}
+            defaultEndDate={defaultEndDate}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="chart-module">
       <div className="report-top-toolbar">
-        <div className="report-toolbar-title">{label[language]}</div>
+        <h4 className="report-toolbar-title">{translatedLabel}</h4>
         <div className="report-button-controls">
           <div>
             <GearIcon width={22} height={22} fill={'#888888'} />
@@ -36,18 +80,46 @@ const ChartModule = (props: ChartModuleProps): JSX.Element => {
               type="checkbox"
               name="styled-checkbox"
               className="styled-checkbox"
-              id={`layer-checkbox-`}
-              checked={true}
-              onChange={(): void => console.log('a')}
+              id={`layer-checkbox-${translatedLabel}`}
+              checked={!submoduleIsHidden}
+              onChange={(): void => setSubmoduleIsHidden(!submoduleIsHidden)}
             />
-            <label className="styled-checkboxlabel" htmlFor={`layer-checkbox-`}>
+            <label
+              className="styled-checkboxlabel"
+              htmlFor={`layer-checkbox-${translatedLabel}`}
+            >
               {'a'}
             </label>
           </div>
         </div>
       </div>
-      <div>Input Controls</div>
-      <div>Chart</div>
+      <div
+        className={
+          submoduleIsHidden ? 'chart-submodule hidden' : 'chart-submodule'
+        }
+      >
+        <div>
+          {currentAnalysis?.uiParams &&
+            currentAnalysis?.uiParams !== 'none' &&
+            currentAnalysis?.uiParams.map((uiParam: any, i: number) => {
+              return (
+                <div className="ui-analysis-wrapper" key={i}>
+                  <div className="ui-description">
+                    <div className="number">
+                      <p>{i + 1}</p>
+                    </div>
+                    <p>{uiParam.label[language]}</p>
+                  </div>
+                  <div className="analysis-input">
+                    {renderInputComponent(uiParam)}
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+        <div>Chart</div>
+        <div>Chart Description</div>
+      </div>
     </div>
   );
 };
