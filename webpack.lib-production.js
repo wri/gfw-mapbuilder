b195ef754999c66255c28aa423285b60eba3d2dc
Vaidotas Piekus <vaidotasp@gmail.com>
2020-06-03 14:11:06 -0400

    wip- library deploy changes to accomodate ARCGIS

--- webpack.lib-production.js
+++ webpack.lib-production.js
@@ -8,20 +8,17 @@ const path = require('path');
 const ArcGISPlugin = require('@arcgis/webpack-plugin');
 const webpack = require('webpack');
 const PACKAGE = require('./package.json');
-const cameroonConfig = require('./configs/countryConfigs/cameroon.js');
+const resources = require('./configs/resources');
 
 module.exports = env => {
   console.log(env);
-  let customConfig = {};
+  const customConfig = resources;
 
   //Generate a public path that is pointing at WRI server appropriate folder corresponding to the folder name that reflects the version, this is done so various esri files
   //like font files and others are loaded correctly due to dynamic pathing issues
   const base =
     'https://wri-sites.s3.amazonaws.com/gfw-mapbuilder.org/library.gfw-mapbuilder.org/';
   const publicPathURL = `${base}${PACKAGE.version}/`;
-  if (env && env.COUNTRY_CONFIG) {
-    customConfig = cameroonConfig; //TODO: Let's make this dynamic!
-  }
 
   return {
     mode: 'production',
@@ -104,7 +101,7 @@ module.exports = env => {
       new HtmlWebPackPlugin({
         title: 'ArcGIS Template Application',
         template: './src/library.html',
-        filename: './library.html',
+        filename: './index.html',
         favicon: './src/assets/favicon.ico',
         chunksSortMode: 'none',
         inlineSource: '.(css)$',
