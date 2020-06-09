a70fdffc6edfed80fc32116d1f431ba984cdcbb8
Vaidotas Piekus <vaidotasp@gmail.com>
2020-06-09 15:09:16 -0400

    static build changes and dynamic config consumption from the entry file

--- webpack.production.js
+++ webpack.production.js
@@ -1,65 +1,131 @@
 //@ts-ignore
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const path = require('path');
+const ArcGISPlugin = require('@arcgis/webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = merge(common, {
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false,
-        terserOptions: {
-          output: {
-            comments: false
+module.exports = () => {
+  return {
+    mode: 'production',
+    entry: {
+      main: ['./src/js/lib.tsx']
+    },
+    output: {
+      filename: '[name].js'
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false,
+          terserOptions: {
+            output: {
+              comments: false
+            }
           }
+        })
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          options: {}
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: ['file-loader']
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: ['url-loader']
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack', 'url-loader']
         }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 20
+      }),
+      new ArcGISPlugin({
+        useDefaultAssetLoaders: false,
+        features: {
+          '3d': false
+        }
+      }),
+
+      new CopyWebpackPlugin([{ from: './configs/configuration.js', to: '' }]),
+
+      new HtmlWebPackPlugin({
+        title: 'ArcGIS Template Application',
+        template: './src/index.html',
+        filename: './index.html',
+        favicon: './src/assets/favicon.ico',
+        chunksSortMode: 'none',
+        inlineSource: '.(css)$'
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: '[name].[chunkhash].css',
+        chunkFilename: '[id].css'
+      }),
+
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|html|css)$/,
+        threshold: 10240
       })
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-
-    new HtmlWebPackPlugin({
-      title: 'ArcGIS Template Application',
-      template: './src/index.html',
-      filename: './index.html',
-      favicon: './src/assets/favicon.ico',
-      chunksSortMode: 'none',
-      inlineSource: '.(css)$'
-    }),
-
-    new MiniCssExtractPlugin({
-      filename: '[name].[chunkhash].css',
-      chunkFilename: '[id].css'
-    }),
-
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.(js|html|css)$/,
-      threshold: 10240
-    })
-  ],
-  resolve: {
-    modules: [
-      path.resolve(__dirname, '/src'),
-      path.resolve(__dirname, 'node_modules/')
     ],
-    extensions: ['.ts', '.tsx', '.js', '.scss', '.css']
-  },
-  node: {
-    process: false,
-    Buffer: false,
-    global: false,
-    fs: 'empty'
-  }
-});
+    resolve: {
+      alias: {
+        js: path.join(__dirname, 'src/js'),
+        css: path.join(__dirname, 'src/css'),
+        images: path.join(__dirname, 'src/images')
+      },
+      modules: [
+        path.resolve(__dirname, '/src'),
+        path.resolve(__dirname, '/configs'),
+        path.resolve(__dirname, 'node_modules/')
+      ],
+      extensions: ['.ts', '.tsx', '.js', '.scss', '.css']
+    },
+    node: {
+      process: false,
+      Buffer: false,
+      global: false,
+      fs: 'empty'
+    }
+  };
+};
