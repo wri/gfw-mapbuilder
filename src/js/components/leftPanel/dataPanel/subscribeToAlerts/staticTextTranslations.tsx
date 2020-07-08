940df46cddc737b2a812f8c280124495d12013a7
Vaidotas Piekus <vaidotasp@gmail.com>
2020-07-08 12:55:00 -0400

    save aoi translations

--- src/js/components/leftPanel/dataPanel/subscribeToAlerts/staticTextTranslations.tsx
+++ src/js/components/leftPanel/dataPanel/subscribeToAlerts/staticTextTranslations.tsx
@@ -15,6 +15,21 @@ export const saveAOIText = {
     save: 'SAVE'
   },
   fr: {
+    title: "Enregistrer une zone d'intérêt",
+    nameLabel: 'Donnez un nom à cette région pour référence ultérieure *',
+    tagsLabel: 'Attribuez des balises pour organiser et regrouper des zones',
+    tagsSubLabel: 'Appuyez sur Entrée pour créer et séparer des balises',
+    required: 'Requis',
+    alertNote:
+      "Nous vous enverrons par e-mail des mises à jour sur les alertes et les modifications de la couverture forestière dans la zone que vous avez sélectionnée, en fonction de votre profil d'utilisateur.",
+    email: 'e-mail',
+    notifications: "SOUHAITEZ-VOUS RECEVOIR DES NOTIFICATIONS D'ALERTE ?",
+    fireDetected: "Dès qu'un incendie est détecté",
+    forestChange: "Dès qu'un changement de forêt est détecté",
+    language: 'LANGUE',
+    save: 'ENREGISTRER'
+  },
+  ka: {
     title: 'Save area of interest',
     nameLabel: 'Name this area for later reference *',
     tagsLabel: 'Assign tags to organize and group areas',
@@ -22,14 +37,6 @@ export const saveAOIText = {
     required: 'Required',
     alertNote:
       'We will send you email updates about alerts and forest cover change in your selected area, based on your user profile.',
-    email: 'e-mail',
-    notifications: 'WOULD YOU LIKE TO RECIEVE ALERT NOTIFICATIONS?',
-    fireDetected: 'As soon as fires are detected',
-    forestChange: 'As soon as forest change is detected',
-    language: 'Language',
-    save: 'SAVE'
-  },
-  ka: {
     email: 'email',
     notifications: 'WOULD YOU LIKE TO RECIEVE ALERT NOTIFICATIONS?',
     fireDetected: 'As soon as fires are detected',
@@ -38,35 +45,64 @@ export const saveAOIText = {
     save: 'SAVE'
   },
   es: {
-    email: 'Correo electrónic',
-    notifications: 'WOULD YOU LIKE TO RECIEVE ALERT NOTIFICATIONS?',
-    fireDetected: 'As soon as fires are detected',
-    forestChange: 'As soon as forest change is detected',
-    language: 'Language',
-    save: 'SAVE'
+    title: 'Guardar zona de interés',
+    nameLabel: 'NOMBRE ESTA ZONA PARA REFERENCIAS POSTERIORES *',
+    tagsLabel: 'ASIGNE ETIQUETAS PARA ORGANIZAR Y AGRUPAR ZONAS',
+    tagsSubLabel: 'Pulse intro para crear y separar las etiquetas',
+    required: 'Obligatorio',
+    alertNote:
+      'Le enviaremos actualizaciones por correo electrónico sobre alertas y cambios en la cobertura forestal en la zona que seleccionó, con base en su perfil de usuario.',
+    email: 'CORREO ELECTRÓNICO',
+    notifications: '¿DESEA RECIBIR NOTIFICACIONES DE ALERTA?',
+    fireDetected: 'Tan pronto como se detecten incendios',
+    forestChange: 'Tan pronto como se detecte un cambio en el bosque',
+    language: 'IDIOMA',
+    save: 'GUARDAR'
   },
   pt: {
-    email: 'email',
-    notifications: 'WOULD YOU LIKE TO RECIEVE ALERT NOTIFICATIONS?',
-    fireDetected: 'As soon as fires are detected',
-    forestChange: 'As soon as forest change is detected',
-    language: 'Language',
-    save: 'SAVE'
+    title: 'Salvar área de interesse',
+    nameLabel: 'NOMEIE ESTA ÁREA PARA REFERÊNCIA FUTURA*',
+    tagsLabel: 'ATRIBUIR MARCADORES PARA ORGANIZAR E AGRUPAR ÁREAS',
+    tagsSubLabel: 'Pressione "Enter" para criar e separar tags',
+    required: 'Requerido',
+    alertNote:
+      'Enviaremos atualizações por e-mail sobre alertas e mudanças na cobertura florestal na sua área selecionada, com base no seu perfil de usuário.',
+    email: 'E-MAIL',
+    notifications: 'VOCÊ GOSTARIA DE RECEBER NOTIFICAÇÕES DE ALERTA?',
+    fireDetected: 'Assim que incêndios forem detectados',
+    forestChange: 'Assim que a mudança florestal for detectada',
+    language: 'IDIOMA',
+    save: 'SALVAR'
   },
   id: {
-    email: 'email',
-    notifications: 'WOULD YOU LIKE TO RECIEVE ALERT NOTIFICATIONS?',
-    fireDetected: 'As soon as fires are detected',
-    forestChange: 'As soon as forest change is detected',
-    language: 'Language',
-    save: 'SAVE'
+    title: 'Simpan daerah yang diminati',
+    nameLabel: 'BERI NAMA DAERAH INI UNTUK REFERENSI DI SAAT SELANJUTNYA *',
+    tagsLabel:
+      'AASIGNE ETIQUETAS PARA ORGANIZAR Y AGRUPAR ZONASSIGNE ETIQUETAS PARA ORGANIZAR Y AGRUPAR ZONAS',
+    tagsSubLabel: 'Tekan enter untuk membuat dan memisahkan tag',
+    required: 'Diperlukan',
+    alertNote:
+      'Kami akan mengirim email kepada Anda tentang peringatan dan perubahan tutupan hutan di daerah yang Anda pilih, berdasarkan profil pengguna Anda.',
+    email: 'EMAIL',
+    notifications: 'APAKAH ANDA INGIN MENERIMA PEMBERITAHUAN PERINGATAN?',
+    fireDetected: 'Segera setelah api terdeteksi',
+    forestChange: 'Segera setelah perubahan hutan terdeteksi',
+    language: 'BAHASA',
+    save: 'SIMPAN'
   },
   zh: {
-    email: '电子邮件',
-    notifications: 'WOULD YOU LIKE TO RECIEVE ALERT NOTIFICATIONS?',
-    fireDetected: 'As soon as fires are detected',
-    forestChange: 'As soon as forest change is detected',
-    language: 'Language',
-    save: 'SAVE'
+    title: '保存关注区域',
+    nameLabel: '命名此区域供以后参考 *',
+    tagsLabel: '为不同的区域分配标签以便进行归纳整理',
+    tagsSubLabel: '按 Enter 键创建并分隔标签',
+    required: '必填',
+    alertNote:
+      '我们将根据您的用户资料向您发送电子邮件，更新您选择的区域的预警和森林覆盖变化情况。',
+    email: '电子邮件 *',
+    notifications: '您是否要接收预警通知？',
+    fireDetected: '一旦检测到火情',
+    forestChange: '一旦检测到森林变化',
+    language: '语言',
+    save: '保存'
   }
 };
