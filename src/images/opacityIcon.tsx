b2d7c03fe79e2cbb719f338cbb66a32a6fdbd29c
Vaidotas Piekus <vaidotasp@gmail.com>
2021-10-29 14:38:59 +0200

    change opacity icon to match flagship

--- src/images/opacityIcon.tsx
+++ src/images/opacityIcon.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const OpacityIcon = (props: any) => {
   return (
     <svg {...props} className="svg-icon report-header__icon report-header__icon-info">
-      <svg {...props} id="shape-info" viewBox="0 0 24 24">
+      <svg {...props} id="shape-info" viewBox="0 0 29 32">
         <title>Opacity Control</title>
-        <path d="M12 3.1L7.05 8.05a7 7 0 1 0 9.9 0L12 3.1zm0-2.828l6.364 6.364a9 9 0 1 1-12.728 0L12 .272zM12 18V8a5 5 0 0 1 0 10z" />
+        <path d="M29.333 7.92c-0.761-1.345-1.646-2.503-2.667-3.526l0 0v23.211c1.040-1.040 1.939-2.224 2.667-3.525v-16.16zM21.333 0.88c-0.752-0.308-1.646-0.577-2.569-0.762l-0.098-0.016v31.797c0.92-0.179 1.813-0.443 2.667-0.779v-30.24zM13.333 32c-7.547-1.181-13.333-7.893-13.333-16s5.787-14.819 13.333-16v32z"></path>
       </svg>
     </svg>
   );
