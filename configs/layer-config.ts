dae6d7dbb7ea6beb21cdd20b923db502c8477c9d
dstarr <dstarr@blueraster.com>
2022-02-23 10:18:05 -0500

    update: moved timeslider default objects into layer config

--- configs/layer-config.ts
+++ configs/layer-config.ts
@@ -30,3 +30,105 @@ export const WRIBasemapConfig = {
 };
 
 export const customBasemapIcon = 'https://my.gfw-mapbuilder.org/img/custom_basemap.png';
+
+//Timeslider objects for tree cover loss and dry spells
+export const defaultMarks: any = {
+  '2000': {
+    label: '2000',
+    style: {}
+  },
+  '2001': {
+    label: '2001',
+    style: { display: 'none' }
+  },
+  '2002': {
+    label: '2002',
+    style: { display: 'none' }
+  },
+  '2003': {
+    label: '2003',
+    style: { display: 'none' }
+  },
+  '2004': {
+    label: '2004',
+    style: { display: 'none' }
+  },
+  '2005': {
+    label: '2005',
+    style: {}
+  },
+  '2006': {
+    label: '2006',
+    style: { display: 'none' }
+  },
+  '2007': {
+    label: '2007',
+    style: { display: 'none' }
+  },
+  '2008': {
+    label: '2008',
+    style: { display: 'none' }
+  },
+  '2009': {
+    label: '2009',
+    style: { display: 'none' }
+  },
+  '2010': {
+    label: '2010',
+    style: {}
+  },
+  '2011': {
+    label: '2011',
+    style: { display: 'none' }
+  },
+  '2012': {
+    label: '2012',
+    style: { display: 'none' }
+  },
+  '2013': {
+    label: '2013',
+    style: { display: 'none' }
+  },
+  '2014': {
+    label: '2014',
+    style: { display: 'none' }
+  },
+  '2015': {
+    label: '2015',
+    style: {}
+  },
+  '2016': {
+    label: '2016',
+    style: { display: 'none' }
+  },
+  '2017': {
+    label: '2017',
+    style: { display: 'none' }
+  },
+  '2018': {
+    label: '2018',
+    style: { display: 'none' }
+  },
+  '2019': {
+    label: '2019',
+    style: { display: 'none' }
+  },
+  '2020': {
+    label: '2020',
+    style: {}
+  }
+};
+export const drySpellMarks = {
+  '2030': {
+    label: '2030',
+    style: {}
+  },
+  '2050': {
+    label: '2050',
+    style: {}
+  },
+  '2080': {
+    label: '2080',
+    style: {}
+  }
+};
--- src/js/components/leftPanel/layersPanel/GenericLayerControl.tsx
+++ src/js/components/leftPanel/layersPanel/GenericLayerControl.tsx
@@ -23,7 +23,7 @@ import {
 import { RootState } from '../../../store';
 import { LayerProps } from '../../../store/mapview/types';
 import { mapController } from '../../../controllers/mapController';
-import { densityEnabledLayers } from '../../../../../configs/layer-config';
+import { defaultMarks, densityEnabledLayers, drySpellMarks } from '../../../../../configs/layer-config';
 import { InfoIcon } from '../../../../images/infoIcon';
 import { DashboardIcon } from '../../../../images/dashboardIcon';
 import { LayerVersionPicker } from './LayerVersionPicker';
@@ -287,106 +287,6 @@ const GenericLayerControl = (props: LayerControlProps): React.ReactElement => {
   const altLayerName = layer.label && layer.label[selectedLanguage];
 
   const returnTimeSlider = (id: string): any => {
-    const defaultMarks: any = {
-      '2000': {
-        label: '2000',
-        style: {}
-      },
-      '2001': {
-        label: '2001',
-        style: { display: 'none' }
-      },
-      '2002': {
-        label: '2002',
-        style: { display: 'none' }
-      },
-      '2003': {
-        label: '2003',
-        style: { display: 'none' }
-      },
-      '2004': {
-        label: '2004',
-        style: { display: 'none' }
-      },
-      '2005': {
-        label: '2005',
-        style: {}
-      },
-      '2006': {
-        label: '2006',
-        style: { display: 'none' }
-      },
-      '2007': {
-        label: '2007',
-        style: { display: 'none' }
-      },
-      '2008': {
-        label: '2008',
-        style: { display: 'none' }
-      },
-      '2009': {
-        label: '2009',
-        style: { display: 'none' }
-      },
-      '2010': {
-        label: '2010',
-        style: {}
-      },
-      '2011': {
-        label: '2011',
-        style: { display: 'none' }
-      },
-      '2012': {
-        label: '2012',
-        style: { display: 'none' }
-      },
-      '2013': {
-        label: '2013',
-        style: { display: 'none' }
-      },
-      '2014': {
-        label: '2014',
-        style: { display: 'none' }
-      },
-      '2015': {
-        label: '2015',
-        style: {}
-      },
-      '2016': {
-        label: '2016',
-        style: { display: 'none' }
-      },
-      '2017': {
-        label: '2017',
-        style: { display: 'none' }
-      },
-      '2018': {
-        label: '2018',
-        style: { display: 'none' }
-      },
-      '2019': {
-        label: '2019',
-        style: { display: 'none' }
-      },
-      '2020': {
-        label: '2020',
-        style: {}
-      }
-    };
-    const drySpellMarks = {
-      '2030': {
-        label: '2030',
-        style: {}
-      },
-      '2050': {
-        label: '2050',
-        style: {}
-      },
-      '2080': {
-        label: '2080',
-        style: {}
-      }
-    };
     switch (id) {
       case 'TREE_COVER_LOSS':
         return (
