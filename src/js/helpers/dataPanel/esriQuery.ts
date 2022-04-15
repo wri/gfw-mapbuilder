7187cf49f28e42e37b4f3f4e31ce1bcbc69befff
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2022-04-14 17:29:25 -0700

    bug: fix timing out bug

--- src/js/helpers/dataPanel/esriQuery.ts
+++ src/js/helpers/dataPanel/esriQuery.ts
@@ -1,17 +1,16 @@
 import { loadModules } from 'esri-loader';
 //Generic ESRI query helper
-export async function esriQuery(
-  url: string,
-  queryParams: any
-): Promise<__esri.FeatureSet> {
-  const [QueryTask, Query] = await loadModules([
-    'esri/tasks/QueryTask',
-    'esri/tasks/support/Query'
-  ]);
+export async function esriQuery(url: string, queryParams: any): Promise<__esri.FeatureSet | any> {
+  const [QueryTask, Query] = await loadModules(['esri/tasks/QueryTask', 'esri/tasks/support/Query']);
   const qt = new QueryTask({
-    url: url
+    url: url,
   });
   const query = new Query(queryParams);
-  const result = qt.execute(query);
-  return result;
+  try {
+    const result = await qt.execute(query);
+    return result;
+  } catch (error) {
+    console.log('error', error);
+    return [];
+  }
 }
