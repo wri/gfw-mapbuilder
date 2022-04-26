2bf7c273a14d6b55c82caa69552265eea2b96260
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2022-04-26 09:05:42 -0700

    refactor: update time slider legend year

--- src/js/store/mapview/reducers.ts
+++ src/js/store/mapview/reducers.ts
@@ -24,7 +24,7 @@ const initialState: MapviewState = {
   activeFeatureIndex: [0, 0], //first element is the index of the layer, second is the index of feature
   documents: null,
   activeBasemap: 'webmap',
-  timeSlider: [2000, 2021],
+  timeSlider: [2001, 2021],
   scale: 0,
   mapCenterCoordinates: { latitude: 0, longitude: 0 },
   layersLoading: true,
