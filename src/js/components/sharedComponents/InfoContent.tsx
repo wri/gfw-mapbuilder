35af6869e025253963765b59ae14a37e3725dc57
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2022-08-01 09:29:16 -0700

    refactor: add caution label

--- src/js/components/sharedComponents/InfoContent.tsx
+++ src/js/components/sharedComponents/InfoContent.tsx
@@ -134,6 +134,7 @@ const InfoContent: FunctionComponent<{}> = (): any => {
     contentDateLabel,
     contentDateRangeLabel,
     citationLabel,
+    cautionLabel,
     licenseLabel,
     downloadDataLabel,
     descriptionLabel,
@@ -327,7 +328,7 @@ const InfoContent: FunctionComponent<{}> = (): any => {
                 />
               </tr>
               <tr>
-                <td className="label">{citationLabel}</td>
+                <td className="label">{cautionLabel}</td>
                 <td className="label-info" dangerouslySetInnerHTML={{ __html: cautions }} />
               </tr>
               <tr>
