e0be4442a13283078509f08084ee91a7c6c18769
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2022-08-04 12:01:45 -0700

    update: webpack config for prod

--- webpack.lib-production.js
+++ webpack.lib-production.js
@@ -7,12 +7,28 @@ const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const PACKAGE = require('./package.json');
+const fs = require('fs');
+const Dotenv = require('dotenv-webpack');
+const dotenv = require('dotenv');
 
-module.exports = env => {
+const currentPath = path.join(__dirname);
+const basePath = currentPath + '/.env';
+// We're concatenating the environment name to our filename to specify the correct env file!
+const envPath = basePath + '.' + process.env.REACT_APP_PLANET_API_KEY;
+// Check if the file exists, otherwise fall back to the production .env
+const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+// Set the path parameter in the dotenv config
+const fileEnv = dotenv.config({ path: finalPath }).parsed;
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+  return prev;
+}, {});
+
+module.exports = (env) => {
   //Generate a public path that is pointing at WRI server appropriate folder corresponding to the folder name that reflects the version, this is done so various esri files
   //like font files and others are loaded correctly due to dynamic pathing issues
-  const base =
-    'https://wri-sites.s3.amazonaws.com/gfw-mapbuilder.org/library.gfw-mapbuilder.org/';
+  const base = 'https://wri-sites.s3.amazonaws.com/gfw-mapbuilder.org/library.gfw-mapbuilder.org/';
   const publicPathURL = `${base}${PACKAGE.version}/`;
 
   return {
@@ -20,18 +36,18 @@ module.exports = env => {
     devtool: 'source-map',
     entry: {
       'library-bundle': ['./src/js/lib.tsx'],
-      [`loader/${PACKAGE.version}`]: [`./src/lib/libLoader.js`]
+      [`loader/${PACKAGE.version}`]: [`./src/lib/libLoader.js`],
     },
     output: {
       filename: '[name].js',
-      publicPath: publicPathURL
+      publicPath: publicPathURL,
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
           loader: 'ts-loader',
-          options: {}
+          options: {},
         },
         {
           test: /\.s[ac]ss$/i,
@@ -41,8 +57,8 @@ module.exports = env => {
             // Translates CSS into CommonJS
             'css-loader',
             // Compiles Sass to CSS
-            'sass-loader'
-          ]
+            'sass-loader',
+          ],
         },
         {
           test: /\.css$/i,
@@ -50,22 +66,22 @@ module.exports = env => {
             // Creates `style` nodes from JS strings
             'style-loader',
             // Translates CSS into CommonJS
-            'css-loader'
-          ]
+            'css-loader',
+          ],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: ['file-loader']
+          use: ['file-loader'],
         },
         {
           test: /\.(png|jpg|gif)$/,
-          use: ['url-loader']
+          use: ['url-loader'],
         },
         {
           test: /\.svg$/,
-          loader: ['file-loader']
-        }
-      ]
+          loader: ['file-loader'],
+        },
+      ],
     },
     optimization: {
       minimizer: [
@@ -75,51 +91,52 @@ module.exports = env => {
           sourceMap: false,
           terserOptions: {
             output: {
-              comments: false
-            }
-          }
-        })
-      ]
+              comments: false,
+            },
+          },
+        }),
+      ],
     },
     plugins: [
       new CleanWebpackPlugin(),
       new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 100
+        maxChunks: 100,
       }),
-
+      new Dotenv(),
+      new webpack.DefinePlugin(envKeys),
       new HtmlWebPackPlugin({
         title: 'ArcGIS Template Application',
         template: './src/library.html',
         filename: './index.html',
         favicon: './src/assets/favicon.ico',
         chunksSortMode: 'none',
-        inlineSource: '.(css)$'
+        inlineSource: '.(css)$',
       }),
 
       new MiniCssExtractPlugin({
         filename: '[name].[chunkhash].css',
-        chunkFilename: '[id].css'
+        chunkFilename: '[id].css',
       }),
 
       new CompressionPlugin({
         filename: '[path].gz[query]',
         algorithm: 'gzip',
         test: /\.(js|html|css)$/,
-        threshold: 10240
-      })
+        threshold: 10240,
+      }),
     ],
     resolve: {
       alias: {
         js: path.join(__dirname, 'src/js'),
         css: path.join(__dirname, 'src/css'),
-        images: path.join(__dirname, 'src/images')
+        images: path.join(__dirname, 'src/images'),
       },
       modules: [
         path.resolve(__dirname, '/src'),
         path.resolve(__dirname, '/configs'),
-        path.resolve(__dirname, 'node_modules/')
+        path.resolve(__dirname, 'node_modules/'),
       ],
-      extensions: ['.ts', '.tsx', '.js', '.scss', '.css']
-    }
+      extensions: ['.ts', '.tsx', '.js', '.scss', '.css'],
+    },
   };
 };
--- webpack.production.js
+++ webpack.production.js
@@ -7,15 +7,32 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
+const Dotenv = require('dotenv-webpack');
+const dotenv = require('dotenv');
+
+const currentPath = path.join(__dirname);
+const basePath = currentPath + '/.env';
+// We're concatenating the environment name to our filename to specify the correct env file!
+const envPath = basePath + '.' + process.env.REACT_APP_PLANET_API_KEY;
+// Check if the file exists, otherwise fall back to the production .env
+const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+// Set the path parameter in the dotenv config
+const fileEnv = dotenv.config({ path: finalPath }).parsed;
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+  return prev;
+}, {});
 
 module.exports = () => {
   return {
     mode: 'production',
     entry: {
-      main: ['./src/js/static.tsx']
+      main: ['./src/js/static.tsx'],
     },
     output: {
-      filename: '[name].js'
+      filename: '[name].js',
     },
     optimization: {
       minimizer: [
@@ -25,18 +42,18 @@ module.exports = () => {
           sourceMap: false,
           terserOptions: {
             output: {
-              comments: false
-            }
-          }
-        })
-      ]
+              comments: false,
+            },
+          },
+        }),
+      ],
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
           loader: 'ts-loader',
-          options: {}
+          options: {},
         },
         {
           test: /\.s[ac]ss$/i,
@@ -46,8 +63,8 @@ module.exports = () => {
             // Translates CSS into CommonJS
             'css-loader',
             // Compiles Sass to CSS
-            'sass-loader'
-          ]
+            'sass-loader',
+          ],
         },
         {
           test: /\.css$/i,
@@ -55,66 +72,67 @@ module.exports = () => {
             // Creates `style` nodes from JS strings
             'style-loader',
             // Translates CSS into CommonJS
-            'css-loader'
-          ]
+            'css-loader',
+          ],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: ['file-loader']
+          use: ['file-loader'],
         },
         {
           test: /\.(png|jpg|gif)$/,
-          use: ['url-loader']
+          use: ['url-loader'],
         },
         {
           test: /\.svg$/,
-          loader: ['file-loader']
-        }
-      ]
+          loader: ['file-loader'],
+        },
+      ],
     },
     plugins: [
       new CleanWebpackPlugin(),
       new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 20
+        maxChunks: 20,
       }),
 
       new CopyWebpackPlugin([
-        { from: './configs/resources.js', to: 'configuration.js' } //TODO: this needs to be somehow dynamic
+        { from: './configs/resources.js', to: 'configuration.js' }, //TODO: this needs to be somehow dynamic
       ]),
-
+      new Dotenv(),
+      new webpack.DefinePlugin(envKeys),
       new HtmlWebPackPlugin({
         title: 'ArcGIS Template Application',
         template: './src/static.html',
         filename: './index.html',
         favicon: './src/assets/favicon.ico',
         chunksSortMode: 'none',
-        inlineSource: '.(css)$'
+        inlineSource: '.(css)$',
       }),
 
       new MiniCssExtractPlugin({
         filename: '[name].[chunkhash].css',
-        chunkFilename: '[id].css'
+        chunkFilename: '[id].css',
       }),
 
       new CompressionPlugin({
         filename: '[path].gz[query]',
         algorithm: 'gzip',
         test: /\.(js|html|css)$/,
-        threshold: 10240
-      })
+        threshold: 10240,
+      }),
     ],
     resolve: {
       alias: {
         js: path.join(__dirname, 'src/js'),
         css: path.join(__dirname, 'src/css'),
-        images: path.join(__dirname, 'src/images')
+        images: path.join(__dirname, 'src/images'),
       },
       modules: [
         path.resolve(__dirname, '/src'),
         path.resolve(__dirname, '/configs'),
-        path.resolve(__dirname, 'node_modules/')
+        path.resolve(__dirname, 'node_modules/'),
       ],
-      extensions: ['.ts', '.tsx', '.js', '.scss', '.css']
-    }
+      extensions: ['.ts', '.tsx', '.js', '.scss', '.css'],
+    },
   };
 };
