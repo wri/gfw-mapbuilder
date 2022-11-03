1b1ac31ecbc0a3b6e23a42ab082dfe2e7bce26a1
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2022-11-03 11:38:08 -0700

    refactor: is value is null then add empty string as value

--- src/js/helpers/dataPanel/formatAttributes.ts
+++ src/js/helpers/dataPanel/formatAttributes.ts
@@ -11,31 +11,39 @@ export function formatAttributeValues(
   Object.keys(attributes).forEach((attribute) => {
     const attributeField = fields?.find((f) => f.fieldName === attribute);
     if (attributeField?.format?.dateFormat) {
-      //format the date if formatting options exist
-      const dateFormatIntlOptions = esriIntl.convertDateFormatToIntlOptions(attributeField?.format?.dateFormat);
-      if (!attributes[attribute]) return;
-      const formattedDate = esriIntl.formatDate(Number(attributes[attribute]), dateFormatIntlOptions);
-      formatAttributeObject[attribute] = formattedDate;
+      if (attributes[attribute] === null || attributes[attribute] === undefined) {
+        formatAttributeObject[attribute] = '';
+      } else {
+        //format the date if formatting options exist
+        const dateFormatIntlOptions = esriIntl.convertDateFormatToIntlOptions(attributeField?.format?.dateFormat);
+        // if (!attributes[attribute]) return;
+        const formattedDate = esriIntl.formatDate(Number(attributes[attribute]), dateFormatIntlOptions);
+        formatAttributeObject[attribute] = formattedDate;
+      }
     } else if (attributeField?.format?.digitSeparator) {
       //format the number if formatting options exist
       const numberFormatIntlOptions = esriIntl.convertNumberFormatToIntlOptions({
         places: attributeField?.format?.places,
         digitSeparator: attributeField?.format?.digitSeparator,
       });
-      if (!attributes[attribute]) return;
-
-      const formattedNumber = esriIntl.formatNumber(attributes[attribute], numberFormatIntlOptions);
-      formatAttributeObject[attribute] = formattedNumber;
+      if (attributes[attribute] === null || attributes[attribute] === undefined) {
+        formatAttributeObject[attribute] = '';
+      } else {
+        const formattedNumber = esriIntl.formatNumber(attributes[attribute], numberFormatIntlOptions);
+        formatAttributeObject[attribute] = formattedNumber;
+      }
     } else {
       //We are doing a string matching against attribute name, this is not ideal and should be addressed on the API level with new formatting options compatible with 4x esri api
       //TODO: This is a short term workaround as proper solution needs to modify all datasets with correct formatting options as described in this issue:
       // https://github.com/wri/gfw-mapbuilder/issues/1051
       if (attribute.toLowerCase().includes('date')) {
         const dateFormatIntlOptions = esriIntl.convertDateFormatToIntlOptions('long-date');
-        if (!attributes[attribute]) return;
-
-        const formatDate = esriIntl.formatDate(attributes[attribute], dateFormatIntlOptions);
-        formatAttributeObject[attribute] = formatDate;
+        if (attributes[attribute] === null || attributes[attribute] === undefined) {
+          formatAttributeObject[attribute] = '';
+        } else {
+          const formatDate = esriIntl.formatDate(attributes[attribute], dateFormatIntlOptions);
+          formatAttributeObject[attribute] = formatDate;
+        }
       } else {
         //no formatting options found,  use original value
         formatAttributeObject[attribute] = attributes[attribute];
