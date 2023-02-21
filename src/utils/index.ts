c7fd0d621c006f3993891351431e83c8f06a0899
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2023-02-21 13:04:39 -0800

    refactor: remove commented out code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,3 @@ export const handleCustomColorTheme = (theme: string | null | undefined) => {
   if (!theme) return '#f0ab00';
   return theme;
 };
-
-export const setToLocalStorage = (key: string, value: any) => {
-  localStorage.setItem(key, value);
-};
