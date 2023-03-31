69559f38cbd4107b6ea3173e65d1354177ef6e64
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2023-03-31 12:14:33 -0700

    fix: issue with dates

--- src/js/components/sharedComponents/DateRangePicker.tsx
+++ src/js/components/sharedComponents/DateRangePicker.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { format } from 'date-fns';
+import { format, add } from 'date-fns';
 import DatePicker from 'react-datepicker';
 import { RootState } from '../../store';
 import { createSelector } from 'reselect';
@@ -18,22 +18,40 @@ export function DateRangePicker() {
   const dispatch = useDispatch();
   const analysisDateRange = useSelector(selectAnalysisDaterange);
 
+  const handleDates = (date, type) => {
+    if (type === 'start') {
+      dispatch(setAnalysisDateRange([format(date, 'yyyy-MM-dd'), analysisDateRange[1]]));
+    } else {
+      dispatch(setAnalysisDateRange([analysisDateRange[0], format(date, 'yyyy-MM-dd')]));
+    }
+  };
+
+  const formatDate = (date) => {
+    const year = new Date(date).getFullYear();
+    const month = new Date(date).getMonth();
+    const dayOfMonth = new Date(date).getDate();
+    const formattedDate = new Date(year, month, dayOfMonth + 1);
+
+    if (formattedDate) return formattedDate;
+    throw new Error('Invalid date DateRangePicker');
+  };
+
   return (
     <div className="calendar-wrapper">
       <div className="date-section-wrapper">
         <label htmlFor="start-date">Start </label>
         <DatePicker
           placeholderText="select a day"
-          onChange={(date: any) => dispatch(setAnalysisDateRange([format(date, 'yyyy-MM-dd'), analysisDateRange[1]]))}
-          selected={new Date(analysisDateRange[0])}
+          onChange={(date: any) => handleDates(date, 'start')}
+          selected={formatDate(analysisDateRange[0])}
         />
       </div>
       <div className="date-section-wrapper">
         <label htmlFor="end-date">End </label>
         <DatePicker
           placeholderText="select a day"
-          onChange={(date: any) => dispatch(setAnalysisDateRange([analysisDateRange[0], format(date, 'yyyy-MM-dd')]))}
-          selected={new Date(analysisDateRange[1])}
+          onChange={(date: any) => handleDates(date, 'end')}
+          selected={formatDate(analysisDateRange[1])}
         />
       </div>
     </div>
