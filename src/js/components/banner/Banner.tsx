84915906247cf4e2323bb4242610d99ff07f5709
davidlstarr <davidlstarr@hotmail.com>
2023-08-16 13:34:58 -0400

    updated banner text

--- src/js/components/banner/Banner.tsx
+++ src/js/components/banner/Banner.tsx
@@ -4,8 +4,9 @@ const Banner = () => {
   return (
     <div className="banner">
       <h1 className="banner__text__title">
-        Trees in Mosaic Landscape dataset will be deprecated as of Aug 2024. Please update your configurations by going
-        to <a href="https://mapbuilder.wri.org/">https://mapbuilder.wri.org/</a>.
+        The Trees in Mosaic Landscapes dataset will be deprecated as of February 2024. Please use our new Tropical Tree
+        Cover dataset instead. Find out more at{' '}
+        <a href="http://mapbuilder.wri.org/tutorials/tml-to-tcc">Replacing Trees in Mosaic Landscapes - MapBuilder </a>
       </h1>
     </div>
   );
