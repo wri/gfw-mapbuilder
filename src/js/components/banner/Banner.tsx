2b7fd2ce314a1db538322f39e047f08a166332b5
davidlstarr <davidlstarr@hotmail.com>
2023-08-17 10:40:08 -0400

    added translations

--- src/js/components/banner/Banner.tsx
+++ src/js/components/banner/Banner.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Banner.scss';
+import { bannerContent } from '../../../../configs/translations/header.translations';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+
 const Banner = () => {
+  const selectedLanguage = useSelector((store: RootState) => store.appState.selectedLanguage);
+  console.log(bannerContent);
+  console.log(selectedLanguage);
+  console.log(bannerContent[selectedLanguage].text);
   return (
     <div className="banner">
       <h1 className="banner__text__title">
-        The Trees in Mosaic Landscapes dataset will be deprecated as of February 2024. Please use our new Tropical Tree
-        Cover dataset instead. Find out more at{' '}
-        <a href="http://mapbuilder.wri.org/tutorials/tml-to-tcc" target="_blank">
-          Replacing Trees in Mosaic Landscapes - MapBuilder{' '}
+        {bannerContent[selectedLanguage].text}{' '}
+        <a href={'http://mapbuilder.wri.org/tutorials/tml-to-tcc'} className="banner__text__link">
+          {bannerContent[selectedLanguage].linkUrlText}
         </a>
       </h1>
     </div>
