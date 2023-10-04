0f2b8dff13fc36c9b9729a843c5d5126fd4df68a
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2023-10-04 14:14:09 -0700

    update: language to use spanish

--- configs/resources.js
+++ configs/resources.js
@@ -16,7 +16,7 @@ export default {
   includeMyGFWLogin: true,
   navLinksInNewTab: false,
   customColorTheme: '',
-  language: 'fr',
+  language: 'es',
   useAlternativeLanguage: true,
   alternativeWebmap: 'de85e3fcc07948238aa6c1afd2a4ceb0',
   alternativeLanguage: 'fr',
