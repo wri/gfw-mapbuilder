d29ab3e84b02ac782e0189f9c2c61b09831c9d93
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2023-10-05 08:30:40 -0700

    update: default language

--- configs/resources.js
+++ configs/resources.js
@@ -16,7 +16,7 @@ export default {
   includeMyGFWLogin: true,
   navLinksInNewTab: false,
   customColorTheme: '',
-  language: 'es',
+  language: 'en',
   useAlternativeLanguage: true,
   alternativeWebmap: 'de85e3fcc07948238aa6c1afd2a4ceb0',
   alternativeLanguage: 'fr',
