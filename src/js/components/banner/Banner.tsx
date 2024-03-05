01b2aea26aaa4a66b765a03918c5575da3499c81
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2024-03-05 09:28:58 -0800

    update: hide template banner

--- src/js/components/banner/Banner.tsx
+++ src/js/components/banner/Banner.tsx
@@ -4,13 +4,19 @@ import { bannerContent } from '../../../../configs/translations/header.translati
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
+// If you want eanble the banner, you need to update ./Banner.scss
 const Banner = () => {
   const selectedLanguage = useSelector((store: RootState) => store.appState.selectedLanguage);
   return (
     <div className="banner">
       <h1 className="banner__text__title">
         {bannerContent[selectedLanguage].text}{' '}
-        <a href={'http://mapbuilder.wri.org/tutorials/tml-to-tcc'} className="banner__text__link" target="_blank">
+        <a
+          href={'http://mapbuilder.wri.org/tutorials/tml-to-tcc'}
+          rel="noreferrer"
+          className="banner__text__link"
+          target="_blank"
+        >
           {bannerContent[selectedLanguage].linkUrlText}
         </a>
       </h1>
