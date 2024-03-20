e93f68c167186f79baaa2c5fce5055d25b53e0c2
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2024-03-20 16:55:12 -0700

    update: add missing translations

--- configs/translations/error.translations.ts
+++ configs/translations/error.translations.ts
@@ -36,31 +36,31 @@ export const errorLayerTranslationTooltip = {
     text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
   },
   az: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Bu lay indi mövcud deyil. Zəhmət olmasa, daha sonra yenidən cəhd edin və ya bu xəritənin inzibatçısı ilə əlaqə saxlayın.',
   },
   nl: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Deze laag is momenteel niet beschikbaar. Probeer het later opnieuw of neem contact op met de beheerder van deze kaart.',
   },
   hy: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Այս շերտը այս պահին անհասանելի է։ Խնդրում ենք կրկին փորձել ավելի ուշ կամ դիմել այս քարտեզի կառավարչին:',
   },
   ka: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'ამ ფენა ამჟამად არ არის ხელმისცი. გთხოვთ, სცადეთ ხელახლა მოგვიანებით ან დაუკავშირდით ამრ نقشის ადმინისტრატორს.',
   },
   fr: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: "Cette couche n'est actuellement pas disponible. Veuillez réessayer plus tard ou contacter l'administrateur de cette carte.",
   },
   es: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Esta capa no está disponible por el momento. Vuelva a intentarlo más tarde o póngase en contacto con el administrador de este mapa.',
   },
   pt: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Esta camada não está disponível no momento. Tente novamente mais tarde ou entre em contato com o administrador deste mapa.',
   },
   id: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Layer ini saat ini tidak tersedia. Silahkan coba lagi nanti atau hubungi administrator peta ini.',
   },
   zh: {
-    text: 'This Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: '此图层目前不可用，请稍后再试或联系此地图的管理员',
   },
 };
 export const errorMetadataTranslationTooltip = {
@@ -68,30 +68,30 @@ export const errorMetadataTranslationTooltip = {
     text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
   },
   az: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Lay haqqında məlumat indi mövcud deyil. Zəhmət olmasa, daha sonra yenidən cəhd edin və ya bu xəritənin inzibatçısı ilə əlaqə saxlayın.',
   },
   nl: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'nformatie over de laag is momenteel niet beschikbaar. Probeer het later opnieuw of neem contact op met de beheerder van deze kaart.',
   },
   hy: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Շերտի մասին տեղեկատվությունը ներկայումս անհասանելի է։ Խնդրում ենք կրկին փորձել ավելի ուշ կամ դիմել այս քարտեզի կառավարչին։',
   },
   ka: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'ფენის ინფორმაცია ამჟამად არ არის ხელმისცი. გთხოვთ, სცადეთ ხელახლა მოგვიანებით ან დაუკავშირდით ამრ نقشის ადმინისტრატორს.',
   },
   fr: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: "Les informations relatives à la couche ne sont actuellement pas disponibles. Veuillez réessayer plus tard ou contacter l'administrateur de cette carte.",
   },
   es: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'La información de la capa no está disponible por el momento. Vuelva a intentarlo más tarde o póngase en contacto con el administrador de este mapa.',
   },
   pt: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'As informações da camada não estão disponíveis no momento. Tente novamente mais tarde ou entre em contato com o administrador deste mapa.',
   },
   id: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: 'Informasi untuk Layer saat ini tidak tersedia. Silahkan coba lagi nanti atau hubungi administrator peta ini.',
   },
   zh: {
-    text: 'Information for Layer is currently unavailable, please try again later or contact the administrator of this map.',
+    text: '有关图层的的信息目前不可用，请稍后再试或联系此地图的管理员。',
   },
 };
