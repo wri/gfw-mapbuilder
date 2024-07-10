b8899c7120a63f555a3bb17242d23edcf174e289
Anthony Amaro <anthonyamaro5555@gmail.com>
2024-07-10 06:48:46 -0700

    update: remove commented out lines

--- configs/modis-viirs.ts
+++ configs/modis-viirs.ts
@@ -4,27 +4,23 @@ export const MODISLayerIDs = [
     layerIds: [21],
     type: 'dynamic',
     url: '',
-    //'https://gis-gfw.wri.org/arcgis/rest/services/Fires/FIRMS_Global_MODIS_48hrs/MapServer/'
   },
   {
     id: 'MODIS72',
     layerIds: [21],
     type: 'dynamic',
     url: '',
-    //'https://gis-gfw.wri.org/arcgis/rest/services/Fires/FIRMS_Global_MODIS_7d/MapServer/'
   },
   {
     id: 'MODIS7D',
     layerIds: [21],
     type: 'dynamic',
     url: '',
-    //'https://gis-gfw.wri.org/arcgis/rest/services/Fires/FIRMS_Global_MODIS_7d/MapServer/'
   },
   {
     id: 'MODIS1Y',
     layerIds: [21],
     type: 'dynamic',
     url: '',
-    //'https://gis-gfw.wri.org/arcgis/rest/services/Fires/FIRMS_Global_MODIS_1yr/MapServer'
   },
 ];
