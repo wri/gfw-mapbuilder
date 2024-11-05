d9159d3a84084ce1a905c27bca4b6eb949d365a6
Anthony Amaro <anthonyamaro5555@gmail.com>
2024-11-05 14:02:42 -0800

    update: migrate to arcgis modules

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -38,8 +38,6 @@ const App = (props: AppSettings | any): JSX.Element => {
   if (resources.analyticsCode) loadDefaultGoogleAnalytics(resources.analyticsCode);
 
   const fetchPortalInfo = async (appID: string) => {
-    //const [Portal, PortalItem] = await loadModules(['esri/portal/Portal', 'esri/portal/PortalItem']);
-
     // APPID existing on the URL indicates that mapbuilder is loaded using arcgis template, and we need to fetch settings using that app id to overwrite our default settings
     const portalURL = sharinghost || 'https://www.arcgis.com';
     const portalA = new Portal({ url: portalURL });
