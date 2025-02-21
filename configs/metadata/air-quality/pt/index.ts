c934b2355e2af8cc4055c554ca75c3c6d0ab1d34
Anthony Amaro <anthonyamaro5555@gmail.com>
2025-02-21 13:10:52 -0800

    feat: add missing translations for air quality layer

---
+++ configs/metadata/air-quality/pt/index.ts
@@ -0,0 +1,51 @@
+export const pt = {
+  title: 'Qualidade do ar: Medições de dióxido de nitrogênio (NO₂) por satélite',
+  subtitle: '3.5 x 5.5 km, global, 2018, TROPOMI/ESA/KNMI/DLR/SRON/BIRA-IASB/STFC/MPIC/S[&]T/Uni-Bremen',
+  download_data: '',
+  learn_more: '',
+  content: [
+    {
+      label: 'Função',
+      value: 'Densidade média mensal de dióxido de nitrogênio (NO₂) na troposfera',
+    },
+    {
+      label: 'Resolução',
+      value: '3.5 x 5.5 km',
+    },
+    {
+      label: 'Cobertura geográfica',
+      value: 'Global',
+    },
+    {
+      label: 'Fonte',
+      value: 'TROPOMI/ESA/KNMI/DLR/SRON/BIRA-IASB/STFC/MPIC/S[&]T/Uni-Bremen',
+    },
+    {
+      label: 'Frequência de atualizações',
+      value: 'Mensal',
+    },
+    {
+      label: 'Dados de conteúdo',
+      value: '2022-05-18 - 2022-06-17',
+    },
+    {
+      label: 'Precauções',
+      value:
+        'A climatologia de albedo de superfície atual usada tem uma resolução espacial de 0,5 ° x 0,5 ° (aproximadamente 55 x 55 km), que é grosseira em comparação com a resolução espacial muito maior usada pelo S5p TROPOMI de 3,5 x 5,5 km. Como consequência, a grade de albedo afeta a qualidade dos produtos da coluna NO₂, especialmente em áreas costeiras. Em geral, o TROPOMI subestima as densidades troposféricas de NO₂ em locais poluídos. Os vieses negativos médios das comparações diárias são geralmente inferiores a 50% (a necessidade do produto para o NO₂ troposférico), mas podem ser bastante variáveis dependendo da estação e do nível de NO₂. Uma boa coerência é encontrada entre os conjuntos de dados TROPOMI e Multi Axis Differential Absorption Spectroscopy (MAX-DOAS) NO₂ com um coeficiente de correlação de 0,84.Para obter mais informações sobre a qualidade dos dados, consulte o [arquivo leiame do produto](https://sentinel.esa.int/documents/247904/3541451/Sentinel-5P-Nitrogen-Dioxide-Level-2-Product-Readme-File).',
+    },
+    {
+      label: 'Licença',
+      value: '[Atribuição necessária](https://sentinel.esa.int/documents/247904/690755/Sentinel_Data_Legal_Notice)',
+    },
+  ],
+  overview: {
+    label: 'Visão geral',
+    value:
+      'O conjunto de dados Qualidade do Ar: Medições de Satélite de NO₂ fornece médias mensais globais de densidade de dióxido de nitrogênio (NO₂) na troposfera. Cada valor mostrado no conjunto de dados representa a densidade de NO₂ entre a superfície da Terra e o topo da troposfera. A densidade de NO₂ é relatada com unidades de moles de NO₂ por metro quadrado de ar (mol/m²). O dióxido de nitrogênio (NO₂) é um dos compostos mais comuns no grupo dos óxidos de nitrogênio (NOx). Outros óxidos de nitrogênio incluem ácido nítrico (HNO₃) e óxido nítrico (NO). O NO₂ é usado como indicador para o grupo maior de óxidos de nitrogênio, o que significa que, se o NO₂ estiver presente, é provável que outros óxidos de nitrogênio também estejam. O NO₂ é criado principalmente pela queima de combustível, que pode ser de carros, caminhões e ônibus, usinas de energia e equipamentos off-road. Respirar ar com alta concentração de NO₂ pode irritar as vias aéreas do sistema respiratório humano. As exposições agudas podem agravar doenças respiratórias, particularmente asma, levando a sintomas respiratórios, como tosse, chiado no peito ou dificuldade para respirar. Exposições crônicas mais longas a concentrações elevadas de NO₂ podem contribuir para o desenvolvimento de asma e potencialmente aumentar a suscetibilidade a infecções respiratórias. Pessoas com asma, assim como crianças e idosos, geralmente correm maior risco de sofrer os efeitos do NO₂ na saúde. O conjunto de dados é composto por dados coletados da missão Sentinel-5 Precursor (S5p), um sistema de satélite polar de órbita baixa da Terra. A missão S5p faz parte do programa de componente espacial de Monitoramento Global do Meio Ambiente e Segurança (GMES/COPERNICUS) liderado pela Comissão Europeia (CE) em parceria com a Agência Espacial Europeia (ESA). Seu objetivo é fornecer informações e serviços sobre qualidade do ar, clima e camada de ozônio. A missão S5p inclui o Instrumento de Monitoramento TROPOsférico (TROPOMI), que faz observações globais diárias dos principais componentes atmosféricos, como o NO₂, com uma resolução de 5,5 x 3,5 quilômetros (km). O Resource Watch também mostra dados TROPOMI para [monóxido de carbono (CO)](https://resourcewatch.org/data/explore/Air-Quality-Measurements-TROPOMI-CO), [ozônio (O₃)](https://resourcewatch.org/data/explore/Air-Quality-Measurements-TROPOMI-O) e [índice de aerossol absorvente (AAI)](https://resourcewatch.org/data/explore/Air-Quality-Measurements-TROPOMI-AER-AI).',
+  },
+  citation: {
+    label: 'Citação',
+    value:
+      'European Space Agency. 2018. ESA Sentinel-5P TROPOMI L3 products. Accessed through Resource Watch, (date). [www.resourcewatch.org](https://www.resourcewatch.org).',
+  },
+};
--- configs/metadata/metadata-config.ts
+++ configs/metadata/metadata-config.ts
@@ -50,28 +50,100 @@ const {
 export const METADATA_CONFIG = {
   [TREE_COVER_LOSS]: {
     en: umdTreeCoverLoss.en,
+    /*  az: umdTreeCoverLoss.az,
+    nl: umdTreeCoverLoss.nl,
+    hy: umdTreeCoverLoss.hy,
+    ka: umdTreeCoverLoss.ka,
+    fr: umdTreeCoverLoss.fr,
+    es: umdTreeCoverLoss.es,
+    pt: umdTreeCoverLoss.pt,
+    id: umdTreeCoverLoss.id,
+    zh: umdTreeCoverLoss.zh, */
   },
   [CARBON_SEQ]: {
     en: co2AccomulationPotential.en,
+    /*  az:co2AccomulationPotential.az,
+    nl:co2AccomulationPotential.nl,
+    hy:co2AccomulationPotential.hy,
+    ka:co2AccomulationPotential.ka,
+    fr:co2AccomulationPotential.fr,
+    es:co2AccomulationPotential.es,
+    pt:co2AccomulationPotential.pt,
+    id:co2AccomulationPotential.id,
+    zh:co2AccomulationPotential.zh, */
   },
   [FOREST_CARBON_NET_FLUX]: {
     en: gfwForestCarbonNetflux.en,
+    /*  az: gfwForestCarbonNetflux.az,
+    nl: gfwForestCarbonNetflux.nl,
+    hy: gfwForestCarbonNetflux.hy,
+    ka: gfwForestCarbonNetflux.ka,
+    fr: gfwForestCarbonNetflux.fr,
+    es: gfwForestCarbonNetflux.es,
+    pt: gfwForestCarbonNetflux.pt,
+    id: gfwForestCarbonNetflux.id,
+    zh: gfwForestCarbonNetflux.zh, */
   },
   [GFW_INTEGRATED_ALERTS]: {
     en: gfwIntegratedAlerts.en,
+    /*  az:gfwIntegratedAlerts.az,
+    nl:gfwIntegratedAlerts.nl,
+    hy:gfwIntegratedAlerts.hy,
+    ka:gfwIntegratedAlerts.ka,
+    fr:gfwIntegratedAlerts.fr,
+    es:gfwIntegratedAlerts.es,
+    pt:gfwIntegratedAlerts.pt,
+    id:gfwIntegratedAlerts.id,
+    zh:gfwIntegratedAlerts.zh, */
   },
   [GLAD_ALERTS]: {
     en: umdLandsatAlerts.en,
+    /*  az: umdLandsatAlerts.az,
+    nl: umdLandsatAlerts.nl,
+    hy: umdLandsatAlerts.hy,
+    ka: umdLandsatAlerts.ka,
+    fr: umdLandsatAlerts.fr,
+    es: umdLandsatAlerts.es,
+    pt: umdLandsatAlerts.pt,
+    id: umdLandsatAlerts.id,
+    zh: umdLandsatAlerts.zh, */
   },
   [INPE_AMAZON_PRODES]: {
     en: inpeAmazonProdes.en,
+    /*  az:inpeAmazonProdes.az,
+    nl:inpeAmazonProdes.nl,
+    hy:inpeAmazonProdes.hy,
+    ka:inpeAmazonProdes.ka,
+    fr:inpeAmazonProdes.fr,
+    es:inpeAmazonProdes.es,
+    pt:inpeAmazonProdes.pt,
+    id:inpeAmazonProdes.id,
+    zh:inpeAmazonProdes.zh, */
   },
   [INPE_CERRADO_PRODES]: {
     en: inpeAmazonProdes.en,
+    /*  az:inpeAmazonProdes.az,
+    nl:inpeAmazonProdes.nl,
+    hy:inpeAmazonProdes.hy,
+    ka:inpeAmazonProdes.ka,
+    fr:inpeAmazonProdes.fr,
+    es:inpeAmazonProdes.es,
+    pt:inpeAmazonProdes.pt,
+    id:inpeAmazonProdes.id,
+    zh:inpeAmazonProdes.zh, */
   },
 
   [TREE_COVER_GAIN]: {
     en: umdTreeCoverGainFromHeight.en,
+    /*  az: umdTreeCoverGainFromHeight.az,
+    nl: umdTreeCoverGainFromHeight.nl,
+    hy: umdTreeCoverGainFromHeight.hy,
+    ka: umdTreeCoverGainFromHeight.ka,
+    fr: umdTreeCoverGainFromHeight.fr,
+    es: umdTreeCoverGainFromHeight.es,
+    pt: umdTreeCoverGainFromHeight.pt,
+    id: umdTreeCoverGainFromHeight.id,
+    zh: umdTreeCoverGainFromHeight.zh, */
   },
 
   [AG_BIOMASS]: {
@@ -89,52 +161,178 @@ export const METADATA_CONFIG = {
 
   [TREE_COVER]: {
     en: treeCover.en,
+    /*  az:treeCover.az,
+    nl:treeCover.nl,
+    hy:treeCover.hy,
+    ka:treeCover.ka,
+    fr:treeCover.fr,
+    es:treeCover.es,
+    pt:treeCover.pt,
+    id:treeCover.id,
+    zh:treeCover.zh, */
   },
   [FOREST_CARBON_GROSS_EMISSIONS]: {
     en: gfwForestCarbonGrossEmissions.en,
+    /*  az: gfwForestCarbonGrossEmissions.az,
+    nl: gfwForestCarbonGrossEmissions.nl,
+    hy: gfwForestCarbonGrossEmissions.hy,
+    ka: gfwForestCarbonGrossEmissions.ka,
+    fr: gfwForestCarbonGrossEmissions.fr,
+    es: gfwForestCarbonGrossEmissions.es,
+    pt: gfwForestCarbonGrossEmissions.pt,
+    id: gfwForestCarbonGrossEmissions.id,
+    zh: gfwForestCarbonGrossEmissions.zh, */
   },
 
   [UMD_LAND_COVER]: {
     en: umdLandCover.en,
+    /*  az:umdLandCover.az,
+    nl:umdLandCover.nl,
+    hy:umdLandCover.hy,
+    ka:umdLandCover.ka,
+    fr:umdLandCover.fr,
+    es:umdLandCover.es,
+    pt:umdLandCover.pt,
+    id:umdLandCover.id,
+    zh:umdLandCover.zh, */
   },
 
   [IFL]: {
     en: intactForestLandscapesChange.en,
+    /*  az: intactForestLandscapesChange.az,
+    nl: intactForestLandscapesChange.nl,
+    hy: intactForestLandscapesChange.hy,
+    ka: intactForestLandscapesChange.ka,
+    fr: intactForestLandscapesChange.fr,
+    es: intactForestLandscapesChange.es,
+    pt: intactForestLandscapesChange.pt,
+    id: intactForestLandscapesChange.id,
+    zh: intactForestLandscapesChange.zh, */
   },
   [LAND_COVER]: {
     en: globalLandcover.en,
+    /*  az:globalLandcover.az,
+    nl:globalLandcover.nl,
+    hy:globalLandcover.hy,
+    ka:globalLandcover.ka,
+    fr:globalLandcover.fr,
+    es:globalLandcover.es,
+    pt:globalLandcover.pt,
+    id:globalLandcover.id,
+    zh:globalLandcover.zh, */
   },
 
   [PRIMARY_FORESTS]: {
     en: regionalPrimaryForest.en,
+    /*  az: regionalPrimaryForest.az,
+    nl: regionalPrimaryForest.nl,
+    hy: regionalPrimaryForest.hy,
+    ka: regionalPrimaryForest.ka,
+    fr: regionalPrimaryForest.fr,
+    es: regionalPrimaryForest.es,
+    pt: regionalPrimaryForest.pt,
+    id: regionalPrimaryForest.id,
+    zh: regionalPrimaryForest.zh, */
   },
 
   [TREE_COVER_HEIGHT]: {
     en: treeCoverHeight.en,
+    /*  az:treeCoverHeight.az,
+    nl:treeCoverHeight.nl,
+    hy:treeCoverHeight.hy,
+    ka:treeCoverHeight.ka,
+    fr:treeCoverHeight.fr,
+    es:treeCoverHeight.es,
+    pt:treeCoverHeight.pt,
+    id:treeCoverHeight.id,
+    zh:treeCoverHeight.zh, */
   },
   [FOREST_CARBON_GROSS_REMOVALS]: {
     en: gfwForestCarbonGrossRemovals.en,
+    /*  az: gfwForestCarbonGrossRemovals.az,
+    nl: gfwForestCarbonGrossRemovals.nl,
+    hy: gfwForestCarbonGrossRemovals.hy,
+    ka: gfwForestCarbonGrossRemovals.ka,
+    fr: gfwForestCarbonGrossRemovals.fr,
+    es: gfwForestCarbonGrossRemovals.es,
+    pt: gfwForestCarbonGrossRemovals.pt,
+    id: gfwForestCarbonGrossRemovals.id,
+    zh: gfwForestCarbonGrossRemovals.zh, */
   },
 
   [VIIRS_ACTIVE_FIRES]: {
     en: viirsFires.en,
+    /*  az:viirsFires.az,
+    nl:viirsFires.nl,
+    hy:viirsFires.hy,
+    ka:viirsFires.ka,
+    fr:viirsFires.fr,
+    es:viirsFires.es,
+    pt:viirsFires.pt,
+    id:viirsFires.id,
+    zh:viirsFires.zh, */
   },
   [TROPICAL_TREE_COVER]: {
     en: wriTreesInMosaicLandscapes.en,
+    /*  az: wriTreesInMosaicLandscapes.az,
+    nl: wriTreesInMosaicLandscapes.nl,
+    hy: wriTreesInMosaicLandscapes.hy,
+    ka: wriTreesInMosaicLandscapes.ka,
+    fr: wriTreesInMosaicLandscapes.fr,
+    es: wriTreesInMosaicLandscapes.es,
+    pt: wriTreesInMosaicLandscapes.pt,
+    id: wriTreesInMosaicLandscapes.id,
+    zh: wriTreesInMosaicLandscapes.zh, */
   },
 
   [DRY_SPELLS]: {
     en: projectedChangeDrySpells.en,
+    /* az:projectedChangeDrySpells.az,
+    nl:projectedChangeDrySpells.nl,
+    hy:projectedChangeDrySpells.hy,
+    ka:projectedChangeDrySpells.ka,
+    fr:projectedChangeDrySpells.fr,
+    es:projectedChangeDrySpells.es,
+    pt:projectedChangeDrySpells.pt,
+    id:projectedChangeDrySpells.id,
+    zh:projectedChangeDrySpells.zh, */
   },
 
   [AIR_QUALITY]: {
     en: airQuality.en,
+    az: airQuality.az,
+    nl: airQuality.nl,
+    hy: airQuality.hy,
+    ka: airQuality.ka,
+    fr: airQuality.fr,
+    es: airQuality.es,
+    pt: airQuality.pt,
+    id: airQuality.id,
+    zh: airQuality.zh,
   },
 
   [WIND_SPEED]: {
     en: windSpeed.en,
+    /* az:windSpeed.az,
+    nl:windSpeed.nl,
+    hy:windSpeed.hy,
+    ka:windSpeed.ka,
+    fr:windSpeed.fr,
+    es:windSpeed.es,
+    pt:windSpeed.pt,
+    id:windSpeed.id,
+    zh:windSpeed.zh, */
   },
   [RECENT_IMAGERY]: {
     en: satelliteImagery.en,
+    /*  az:satelliteImagery.az,
+    nl:satelliteImagery.nl,
+    hy:satelliteImagery.hy,
+    ka:satelliteImagery.ka,
+    fr:satelliteImagery.fr,
+    es:satelliteImagery.es,
+    pt:satelliteImagery.pt,
+    id:satelliteImagery.id,
+    zh:satelliteImagery.zh, */
   },
 };
