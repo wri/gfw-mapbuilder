60ed4b3dc80b48d22485781983503bb3298e2e03
Anthony Amaro <anthonyamaro5555@gmail.com>
2025-02-28 13:47:32 -0800

    feat: add metadata translations for gfw integrated alerts

---
+++ configs/metadata/gfw-integrated-alerts/az/index.ts
@@ -0,0 +1,52 @@
+export const az = {
+  title: 'İnteqrasiya edilmiş meşələrin qırılması xəbərdarlığı',
+  subtitle: 'gündəlik, 10m, tropik, UMD/GLAD və WUR',
+  download_data: 'https://data.globalforestwatch.org/datasets/gfw::integrated-deforestation-alerts/about',
+  content: [
+    {
+      label: 'Funksiya',
+      value:
+        'Üç xəbərdarlıq sistemindən inteqrasiya edilmiş xəbərdarlıqlardan istifadə edərək, yaxın vaxtda meşələrin pozulmasına nəzarət edin',
+    },
+    {
+      label: 'Qətmə',
+      value: '10 × 10 m',
+    },
+    {
+      label: 'Coğrafi əhatə',
+      value: '30°Ş.-dən 30°C-yə qədər',
+    },
+    {
+      label: 'Mənbə',
+      value:
+        'GLAD Xəbərdarlıqları:\nHansen, M.C., A.Krylov, A.Tyukavina, P.V. Potapov, S. Turubanova, B. Zutta, S. Ifo, B. Margono, F. Stolle və R. Moore. 2016. Landsat datasından istifadə edərək rütubətli tropik meşələrin pozulması barədə xəbərdarlıqlar. Ətraf Mühitin Tədqiqi Məktubları, 11 (3). (https://dx.doi.org/10.1088/1748-9326/11/3/034008)[https://dx.doi.org/10.1088/1748-9326/11/3/034008]',
+    },
+    {
+      label: 'Tezlik',
+      value: 'Gündəlik',
+    },
+    {
+      label: 'Məzmun tarixi',
+      value: '1 yanvar 2019-cu il',
+    },
+    {
+      label: 'Xəbərdarlıqlar',
+      value:
+        '“Meşələrin qırılması xəbərdarlığı” adlandırılsa da, bu xəbərdarlıqlar meşə və ya ağac örtüyünün pozulmasını aşkar edir. Bu məhsul insan tərəfindən törədilən və digər narahatlıq növləri arasında fərq qoymur. Xəbərdarlıqlar əkin meşələrində aşkar edildikdə (GLAD-L sistemində baş vermə ehtimalı daha yüksəkdir), xəbərdarlıqlar meşədən kənar torpaq istifadəsinə çevrilmədən ağac yığımı əməliyyatlarını göstərə bilər. \nMeşələrin qırılması termini istifadə olunur, çünki bunlar potensial meşələrin qırılması hadisələridir və bunu müəyyən etmək üçün xəbərdarlıqlar əlavə olaraq araşdırıla bilər. \nQlobal və ya regional tendensiyaların qiymətləndirilməsi və ya ərazinin təxminləri üçün meşələrin qırılması xəbərdarlığından istifadə etməyi tövsiyə etmirik. Zamanla meşələrin dəyişməsi tendensiyalarının daha dəqiq müqayisəsi və ərazi təxminləri üçün ağac örtüyü itkisi ilə bağlı illik məlumatlardan istifadə etməyi tövsiyə edirik. Son xəbərdarlıqlara hələ öz etimad səviyyəsini yüksəltməmiş və nəticədə silinə bilən yanlış pozitivlər daxildir. 6 ay ərzində örtülməmiş əlavə peyk müşahidələrindən əvvəl örtünün sürətli bağlanması baş verərsə, keçmiş xəbərdarlıqlar səhvən verilənlər bazasından silinmiş ola bilər. Əlavə olaraq, metodologiyaların yenilənməsi, müxtəlif sistemlərin sayı (inteqrasiya edilmiş xəbərdarlıqlar halında) və bulud örtüyünün aylar və illər arasında dəyişməsi meşələrin qırılması xəbərdarlığının illik/illərarası müqayisə üçün istifadə edilməsində əlavə risklər yaradır.\nXəbərdarlıqlar istifadəçini maraqlandıran xəbərdarlıqları, məsələn, meşələrin qırılması ehtimalı olan və fəaliyyət üçün prioritetləşdirilə bilən xəbərdarlıqları müəyyən etmək üçün "təmizlənə" bilər. İstifadəçi bunu qorunan ərazilər və ya əkilmiş ağaclar kimi digər kontekstual məlumat dəstlərinin üst-üstə düşməsi ilə edə bilər. İstifadəçilərin öz prioritetləşdirmə yanaşmalarını müəyyən edə bilməsi üçün qeyri-kurasiya edilmiş məlumatlar burada təqdim olunur. Seçilmiş xəbərdarlıq yerləri Baxılacaq Yerlər məlumat qatında təmin edilir.\nÜç xəbərdarlıq sistemində meşə/ağac örtüyü və meşə/ağac örtüyünün pozulmasına dair müxtəlif təriflər var: \n\n\nGLAD-L: xəbərdarlıqlar hündürlüyü 5 metrdən çox olan bütün bitki örtüyünün 60%-dən çox örtüyü ilə müəyyən edilən “ağac örtüyü” daxilindədir və təbii meşələr və ya plantasiyalar şəklində ola bilər. “Ağac örtüyünün itirilməsi” ən azı yarım pikselin örtüyünün çıxarılmasını göstərir və mexaniki yığım, yanğın, xəstəlik və ya fırtına zədəsi də daxil olmaqla müxtəlif amillərə görə ola bilər. Beləliklə, "itki" meşələrin qırılmasına bərabər deyil. \nGLAD-S2: xəbərdarlıqlar Amazon çayı hövzəsində Turubanova və digərlərinin (2018) əsas meşə maskası daxilindədir, Hansen və digərlərindən 2001-ci ildən indiki meşə itkisi. (2013) çıxarıldı. \nRADD: xəbərdarlıqlar ilkin rütubətli meşələr daxilindədir. Meşə itkisi bir piksel daxilində ağac örtüyünün tam və ya qismən çıxarılması kimi müəyyən edilir və minimum 0,5 ha-lıq xəritəçəkmə vahidindən istifadə edilir. \nGiriş xəbərdarlığı sistemləri eyni məkan və müvəqqəti əhatə dairəsinə malik deyil:\nGLAD-L: Amazon, Konqo hövzəsi və Cənub-Şərqi Asiyadakı seçilmiş ölkələr üçün 2018-ci il yanvarın 1-dən indiyədək və 2015-ci ildən bu günə qədər (2022-ci ildə bir müddət fasilə verilmiş olsa da) bütün tropiklərdə (30°Ş.-dən 30°S-ə qədər) fəaliyyət göstərir. \nGLAD-S2: 2019-cu ilin yanvarından indiyədək Cənubi Amerikanın əsas nəmli tropik meşə ərazilərində fəaliyyət göstərir \nRADD: 2019-cu ilin yanvarından indiyədək Afrika üçün və 2020-ci ilin yanvarından indiyədək Cənubi Amerika və Cənub-Şərqi Asiyanı əhatə edən Cənubi Amerika, Saharadan cənub-şərqi Afrika və Cənub-Şərqi Asiyanın əsas rütubətli tropik meşə sahələrində fəaliyyət göstərir, 2023-cü ilin yanvarından Mərkəzi Amerikanı əhatə edir (kontinental SE Asiya və Sakit Okean 02 tərəfindən genişlənmə) \n\nÜç xəbərdarlıq sistemini ümumi şəbəkəyə inteqrasiya etmək üçün GLAD-L, GLAD-S2 və RADD-ə uyğunlaşdırmaq üçün 30 m məkan ayırdetməsindən 10 m-ə qədər yenidən nümunə götürülür. Nəticədə, vahid 30 m GLAD-L pikseli inteqrasiya olunmuş təbəqədə çoxsaylı 10 m pikselə çevriləcək. İstifadəçilər ayrı-ayrı sistemlərin təhlil nəticələrini inteqrasiya olunmuş xəbərdarlıq səviyyəsi ilə müqayisə edərkən ehtiyatlı davranmalıdırlar, çünki inteqrasiya olunmuş xəbərdarlıqların sayı yerli GLAD-L siqnallarının sayından xeyli çox olacaq. Bundan əlavə, inteqrasiya olunmuş təbəqədəki piksellər bu yenidən nümunələmə nəticəsində xəritədə fərdi GLAD-L təbəqəsindəki piksellərlə tam uyğunlaşmaya bilər. \nİnteqrasiya edilmiş təbəqədəki hər bir piksel, hətta birdən çox sistem həmin pikseldə xəbərdarlıq bildirsə belə, istənilən xəbərdarlıq sistemindən aşkarlanmanın ən erkən tarixini saxlayır. Bəzi hallarda, inteqrasiya olunmuş təbəqədən fərdi xəbərdarlıq sistemi təbəqələrinə keçid zamanı bu, uyğun olmayan vizuallaşdırmalara səbəb ola bilər. Hər hansı bir xəbərdarlıq sistemi tərəfindən aşkarlanmanın ən erkən tarixi ilə maraqlandığınız zaman inteqrasiya olunmuş təbəqədən istifadə etmək məsləhətdir. Bununla belə, müəyyən bir xəbərdarlıq növü ilə maraqlanırsınızsa, fərdi xəbərdarlıq sisteminin təbəqələrindən istifadə etmək daha yaxşıdır. \n“Ən yüksək etibarlılıq: birdən çox xəbərdarlıq sistemi tərəfindən aşkar edilmişdir” səviyyəsinə yalnız həmin bölgə üçün birdən çox xəbərdarlıq sisteminin işlədiyi ərazilərdə və müddətlərdə nail olmaq olar. \nMənbə məlumatları yeniləndikcə güvən səviyyəsi geriyə doğru dəyişə bilər; 180 gün ərzində yüksək etibarlılığa çevrilməyən xəbərdarlıqlar verilənlər bazasından silinir.\nXəbərdarlıq pikseli yüksək etibarlılığa çatdıqdan sonra meşə itkisi həmin yerdə yenidən eyni xəbərdarlıq sistemi tərəfindən aşkar edilməyəcək\nDəqiqliklər üç xəbərdarlıq sisteminin müxtəlif xüsusiyyətlərinə görə inteqrasiya edilmiş siqnalların əhatə dairəsi üzrə dəyişir – Radar (RADD) xəbərdarlığı, məsələn, qısa dalğa uzunluğunda olan C-zolağı radarın nəmlik dəyişkənliyinə yüksək həssaslığı səbəbindən bataqlıq meşələrində daha çox yanlış aşkarlamalara malik ola bilər.\nBöyüdüldükdə, bu məlumat təbəqəsi müəyyən dərəcədə qeyri-dəqiqlik göstərir, çünki məlumat nöqtələri daha böyük miqyasda görünmək üçün yığışdırılmalıdır. Daha ətraflı məlumat üçün böyüdün.',
+    },
+    {
+      label: 'Lisenziya',
+      value: 'CC 4.0',
+    },
+  ],
+  overview: {
+    label: 'Ümumi baxış',
+    value:
+      'Qlobal Forest Watch tərəfindən yığılmış bu məlumat dəsti üç xəbərdarlıq sistemindən (GLAD-L, GLAD-S2, RADD) meşələrin qırılması ilə bağlı xəbərdarlıqları vahid, inteqrasiya olunmuş meşələrin qırılması xəbərdarlığı səviyyəsinə birləşdirir. Bu inteqrasiya istifadəçilərə meşələrin qırılması hadisələrini tək hər hansı bir sistemdən daha tez aşkar etməyə imkan verir, çünki mənbə xəbərdarlığı sistemlərindən hər hansı biri yeniləndikdə inteqrasiya olunmuş təbəqə yenilənir.',
+  },
+  citation: {
+    label: 'Sitat',
+    value:
+      'Mənbə: "İnteqrasiya edilmiş meşələrin qırılması xəbərdarlığı". UMD/GLAD və WUR, Global Forest Watch vasitəsilə əldə edilir',
+  },
+};
