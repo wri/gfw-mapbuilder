af502e2d47064c73bfb373cc70d4318e1b482c0d
Anthony Amaro <anthonyamaro5555@gmail.com>
2025-03-03 07:59:31 -0800

    feat: add metadata translations for regional primary forest

---
+++ configs/metadata/regional-primary-forest/es/index.ts
@@ -0,0 +1,40 @@
+export const es = {
+  title: 'Bosques primarios',
+  subtitle: '30 m, trópicos, UMD/GLAD',
+  download_data: 'https://glad.umd.edu/dataset/primary-forest-humid-tropics',
+  learn_more: 'https://glad.umd.edu/dataset/primary-forest-humid-tropics',
+  content: [
+    {
+      label: 'Función',
+      value:
+        'Este conjunto de datos representa la extensión de los bosques primarios en las regiones pantropicales del mundo en 2001.',
+    },
+    {
+      label: 'Resolución',
+      value: '30 × 30 metros',
+    },
+    {
+      label: 'Cobertura geográfica',
+      value: 'Pan-tropical',
+    },
+    {
+      label: 'Fuente',
+      value:
+        'Turubanova, S., Potapov, P.V., Tyukavina, A. and Hansen, M.C., 2018. Ongoing primary forest loss in Brazil, Democratic Republic of the Congo, and Indonesia. Environmental Research Letters, 13(7), p.074028.',
+    },
+    {
+      label: 'Fecha del contenido',
+      value: 2001,
+    },
+  ],
+  overview: {
+    label: 'Descripción',
+    value:
+      'Los bosques primarios se encuentran entre los bosques más biodiversos, proporcionando una multitud de servicios ecosistémicos, lo que hace crucial el monitoreo de su planeación de uso de suelo y contabilidad de carbono a nivel nacional. Este conjunto de datos define bosques primarios como "superficies forestales tropicales húmedas naturales maduras que no han sido despejadas y reforestadas completamente en la historia reciente".',
+  },
+  citation: {
+    label: 'Citación',
+    value:
+      'Turubanova, S., Potapov, P.V., Tyukavina, A. and Hansen, M.C., 2018. Ongoing primary forest loss in Brazil, Democratic Republic of the Congo, and Indonesia. Environmental Research Letters, 13(7), p.074028. Accessed through Global Forest Watch on 31/01/2025. www.globalforestwatch.org.',
+  },
+};
--- configs/metadata/regional-primary-forest/index.ts
+++ configs/metadata/regional-primary-forest/index.ts
@@ -1,4 +1,13 @@
 import { en } from './en';
+import { az } from './az';
+import { es } from './es';
+import { fr } from './fr';
+import { hy } from './hy';
+import { id } from './id';
+import { ka } from './ka';
+import { nl } from './nl';
+import { pt } from './pt';
+import { zh } from './zh';
 
-const regionalPrimaryForest = { en };
+const regionalPrimaryForest = { en, az, es, fr, hy, id, ka, nl, pt, zh };
 export default regionalPrimaryForest;
