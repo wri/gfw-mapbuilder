2d1df8b78c5e6f43eea88ec319225702a83e233f
Anthony <aamaro@Anthonys-MacBook-Pro.local>
2025-03-28 08:09:47 -0700

    update: add link to a tag

--- src/js/components/banner/Banner.tsx
+++ src/js/components/banner/Banner.tsx
@@ -5,18 +5,14 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
 // If you want eanble the banner, you need to update ./Banner.scss
+const LINK_URL = 'https://www.globalforestwatch.org/blog/data-and-tools/planet-imagery-changes-gfw/';
 const Banner = () => {
   const selectedLanguage = useSelector((store: RootState) => store.appState.selectedLanguage);
   return (
     <div className="banner">
       <h1 className="banner__text__title">
         {bannerContent[selectedLanguage].text}{' '}
-        <a
-          href={'http://mapbuilder.wri.org/tutorials/tml-to-tcc'}
-          rel="noreferrer"
-          className="banner__text__link"
-          target="_blank"
-        >
+        <a href={LINK_URL} rel="noreferrer" className="banner__text__link" target="_blank">
           {bannerContent[selectedLanguage].linkUrlText}
         </a>
       </h1>
