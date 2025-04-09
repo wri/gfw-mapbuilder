2d51067678025b5877bfe5ff3f3ab40e478ed477
Anthony <aamaro@Anthonys-MacBook-Pro.local>
2025-04-09 12:56:52 -0700

    feat: add forest carbon removals layer config

--- configs/layers/layers-content-config.ts
+++ configs/layers/layers-content-config.ts
@@ -1,5 +1,13 @@
 import projectedChangeDrySpells from './protected-change-dry-spells';
 import potentialCarbonSequestrationRate from './potential-carbon-sequestration-rate';
 import airQuality from './air-quality';
+import windSpeed from './wind-speed';
+import forestCarbonGrossRemovals from './forest-carbon-gross-removals';
 
-export const layersContentConfig = [projectedChangeDrySpells, potentialCarbonSequestrationRate, airQuality] as any;
+export const layersContentConfig = [
+  projectedChangeDrySpells,
+  potentialCarbonSequestrationRate,
+  airQuality,
+  windSpeed,
+  forestCarbonGrossRemovals,
+] as any;
